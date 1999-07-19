631c43d77a85d22f6c8ec4baad00f3b3063ac43c
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
1999-07-19 07:44:09 +0000

    Initial revision
    git-svn-id: file:///usr/local/svn/fsc2@49 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/src/global.h
@@ -0,0 +1,131 @@
+#if ! defined FSC2_GLOBAL
+#define FSC2_GLOBAL
+
+
+
+/* make bool synonymous to unsigned char */
+
+typedef  unsigned char  bool;
+
+
+
+/* Define the maximum number of pulses (thus pulse numbers have to be in
+   the range between 0 and MAX_PULSE_NUM - 1) */
+
+#define MAX_PULSE_NUM  400
+
+
+/* Define the maximum length of phase sequences */
+
+#define MAX_PHASE_SEQ_LEN  12    
+
+
+
+/* Define some useful abbreviations - never ever change these ! */
+
+#define	FAIL  ( ( bool ) 0 )      
+#define UNSET ( ( bool ) 0 )
+#define OK    ( ( bool ) 1 )
+#define SET   ( ( bool ) 1 )
+
+
+
+/* Define error severity types */
+
+enum {
+	   FATAL = 0,
+	   SEVERE,
+	   WARN,
+	   NO_ERROR
+};
+
+
+/* Define the different section types */
+
+enum {
+	  NO_SECTION = ( int ) OK + 1,
+	  ASSIGNMENTS_SECTION,
+	  DEFAULTS_SECTION,
+	  VARIABLES_SECTION,
+	  PHASES_SECTION,
+	  PREPARATIONS_SECTION,
+	  EXPERIMENT_SECTION
+};
+
+
+
+/* Define the different functions pulses can have */
+
+enum {
+	PULSER_CHANNEL_NO_TYPE = -1,
+	PULSER_CHANNEL_MW,
+    PULSER_CHANNEL_TWT,
+	PULSER_CHANNEL_TWT_GATE,
+	PULSER_CHANNEL_DET,
+	PULSER_CHANNEL_DET_GATE,
+	PULSER_CHANNEL_RF,
+	PULSER_CHANNEL_RF_GATE,
+	PULSER_CHANNEL_PHASE_X,
+	PULSER_CHANNEL_PHASE_Y
+};
+
+
+/* Define access types for functions in certain sections */
+
+enum {
+	  ACCESS_RESTRICTED,
+	  ACCESS_ALL_SECTIONS
+};
+
+
+
+/* Define the types of phases used in phase cycling */
+
+enum {
+	   PHASE_PLUS_X,
+	   PHASE_MINUS_X,
+	   PHASE_PLUS_Y,
+	   PHASE_MINUS_Y
+};
+
+
+/* Define the acquisition types used in phase cycling */
+
+enum {
+	   ACQ_PLUS,
+	   ACQ_MINUS
+};
+
+
+/* Define the different types of variables */
+
+enum {
+	UNDEF_VAR = -1,
+	INT_VAR,
+	FLOAT_VAR,
+	INT_ARR,
+	FLOAT_ARR,
+	FUNC,
+};
+
+
+
+
+/* Define a structure used in analyzing the input program */
+
+typedef struct
+{
+	/* global compilation error states (FATAL, SEVERE and WARN) */
+
+	bool error[ 3 ];
+
+	/* indicates which sections have already been handled to detect
+	   multiple instances of the same section */
+
+	bool sections[ EXPERIMENT_SECTION + 1 ];
+} Compilation;
+
+
+
+
+#endif FSC2_GLOBAL
