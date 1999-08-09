f95ee6217d20f9895f2cc5715f00f07a90d1c0b9
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
1999-08-09 21:30:21 +0000

    Initial revision
    git-svn-id: file:///usr/local/svn/fsc2@112 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/src/exp.h
@@ -0,0 +1,18 @@
+/*
+  $Id$
+*/
+
+
+
+#if ! defined PRIM_EXP_HEADER
+#define PRIM_EXP_HEADER
+
+#include "fsc2.h"
+
+
+bool store_exp( FILE *in );
+void forget_prg( void );
+void prim_check( void );
+
+
+#endif   /* ! PRIM_EXP_HEADER */
