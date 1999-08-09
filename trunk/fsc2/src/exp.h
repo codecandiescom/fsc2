fad2751c6bc23eaf89cfe1c175781bea9e886ee7
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
1999-08-09 22:03:23 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@113 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/exp.h
+++ trunk/fsc2/src/exp.h
@@ -2,12 +2,19 @@
   $Id$
 */
 
-
-
 #if ! defined PRIM_EXP_HEADER
 #define PRIM_EXP_HEADER
 
 #include "fsc2.h"
+
+
+#define IF_TOK     2049
+#define ELSE_TOK   2050
+#define ENDIF_TOK  2051
+#define WHILE_TOK  2052
+#define DONE_TOK   2053
+#define CONT_TOK   2054
+#define BREAK_TOK  2055
 
 
 bool store_exp( FILE *in );
