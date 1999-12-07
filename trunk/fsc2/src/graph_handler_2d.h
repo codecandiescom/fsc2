66f2ed1db1b3fee83bd16719d5244e5d2837242c
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
1999-12-07 17:21:49 +0000

    Initial revision
    git-svn-id: file:///usr/local/svn/fsc2@556 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/src/graph_handler_2d.h
@@ -0,0 +1,24 @@
+/*
+  $Id$
+*/
+
+
+#if ! defined GRAPH_HANDLER_D__HEADER
+#define GRAPH_HANDLER_2D_HEADER
+
+
+#include "fsc2.h"
+
+
+int canvas_handler_2d( FL_OBJECT *obj, Window window, int w, int h, XEvent *ev,
+					   void *udata );
+void reconfigure_window_2d( Canvas *c, int w, int h );
+void recalc_XPoints_2d( void );
+void recalc_XPoints_of_curve_2d( Curve_2d *cv );
+void redraw_all_2d( void );
+void redraw_canvas_2d( Canvas *c );
+void repaint_canvas_2d( Canvas *c );
+void fs_rescale_2d( void );
+
+
+#endif   /* ! GRAPH_HANDLER_2D_HEADER */
