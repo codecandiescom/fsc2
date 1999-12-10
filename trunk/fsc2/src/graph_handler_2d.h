f1c478a4d3a7a0d048aee39470b0459de6e9703d
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
1999-12-10 08:33:49 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@570 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/graph_handler_2d.h
+++ trunk/fsc2/src/graph_handler_2d.h
@@ -20,6 +20,7 @@ void redraw_canvas_2d( Canvas *c );
 void repaint_canvas_2d( Canvas *c );
 void fs_rescale_2d( Curve_2d *cv );
 void make_scale_2d( Curve_2d *cv, Canvas *c, int coord );
+void save_scale_state_2d( Curve_2d *cv );
 
 
 #endif   /* ! GRAPH_HANDLER_2D_HEADER */
