6a044a0c4419f4db5b16d15a56f0d7c0547aecaa
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-02-02 17:37:25 +0000

    Initial revision
    git-svn-id: file:///usr/local/svn/fsc2@718 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/modules/dg2020_f.h
@@ -0,0 +1,168 @@
+/*
+ $Id$
+*/
+
+
+#include "fsc2.h"
+
+
+#define Ticks long
+
+
+#define MIN_TIMEBASE            5.0e-9     /* 5 ns */
+#define MAX_TIMEBASE            0.1        /* 0.1 s */
+
+#define MAX_PODS                12
+#define MAX_CHANNELS            36
+
+
+# define MIN_POD_HIGH_VOLTAGE  -2.0  /* Data for P3420 (Variable Output Pod) */
+# define MAX_POD_HIGH_VOLTAGE   7.0
+
+# define MIN_POD_LOW_VOLTAGE   -3.0
+# define MAX_POD_LOW_VOLTAGE    6.0
+
+#define MAX_POD_VOLTAGE_SWING   9.0
+#define MIN_POD_VOLTAGE_SWING   0.5
+
+#define VOLTAGE_RESOLUTION      0.1
+
+#define MAX_TRIG_IN_LEVEL       5.0
+#define MIN_TRIG_IN_LEVEL      -5.0
+
+
+
+#define MIN_BLOCK_SIZE     64     /* minimum length of block */
+#define MAX_BLOCK_REPEATS  65536  /* maximum of repeats of block in sequence */
+#define MAX_PULSER_BITS    65536  /* maximum number of bits in channel */
+
+#define NO_PULSE           -1     /* marks unused pulse numbers */
+#define NO_CHANNEL         -1     /* marks unused channels */
+
+
+
+typedef struct {
+	int self;
+
+	bool is_used;
+
+	struct _P_ *pod;
+	bool is_inverted;
+
+	int num_channels;
+	struct _C_ *channel[ MAX_CHANNELS ];
+
+	Ticks delay;
+	bool is_delay;
+
+	double high_level;
+	double low_level;
+	bool is_high_level;
+	bool is_low_level;
+} FUNCTION;
+
+
+typedef struct _P_ {
+	FUNCTION *function;
+} POD;
+
+
+typedef struct _C_ {
+	FUNCTION *function;
+} CHANNEL;
+
+
+typedef struct
+{
+	double timebase;
+	bool is_timebase;
+
+	int trig_in_mode;        /* EXTERNAL or INTERNAL */
+	int trig_in_slope;       /* only in EXTERNAL mode */
+	double trig_in_level;    /* only in EXTERNAL mode */
+	Ticks repeat_time;       /* only in INTERNAL mode */
+
+	bool is_trig_in_mode;
+	bool is_trig_in_slope;
+	bool is_trig_in_level;
+	bool is_repeat_time;
+
+	FUNCTION function[ PULSER_CHANNEL_NUM_FUNC ];
+	POD pod[ MAX_PODS ];
+	CHANNEL channel[ MAX_CHANNELS ];
+
+} DG2020;
+
+
+typedef struct _p_ {
+
+	long num;
+
+	struct _p_ *next;
+	struct _p_ *prev;
+
+	FUNCTION *function;
+	Ticks pos;
+	Ticks len;
+	Ticks dpos;
+	Ticks dlen;
+	Ticks maxlen;
+	long num_repl;
+	long *repl_list;
+
+	bool is_function;
+	bool is_pos;
+	bool is_len;
+	bool is_dpos;
+	bool is_dlen;
+	bool is_maxlen;
+
+	Ticks initial_pos;
+	Ticks initial_len;
+	Ticks initial_dpos;
+	Ticks initial_dlen;
+
+} PULSE;
+
+
+
+int dg2020_init_hook( void );
+int dg2020_test_hook( void );
+int dg2020_exp_hook( void );
+void dg2020_exit_hook( void );
+
+
+static bool set_timebase( double timebase );
+static bool assign_function( int function, long pod );
+static bool assign_channel_to_function( int function, long channel );
+static bool invert_function( int function );
+static bool set_delay_function( int function, double delay );
+static bool set_function_high_level( int function, double voltage );
+static bool set_function_low_level( int function, double voltage );
+static bool set_trigger_mode( int mode );
+static bool set_trig_in_level( double voltage );
+static bool set_trig_in_slope( int slope );
+static bool set_repeat_time( double time );
+
+static bool new_pulse( long pnum );
+static bool set_pulse_function( long pnum, int function );
+static bool set_pulse_position( long pnum, double time );
+static bool set_pulse_length( long pnum, double time );
+static bool set_pulse_position_change( long pnum, double time );
+static bool set_pulse_length_change( long pnum, double time );
+static bool set_pulse_maxlen( long pnum, double time );
+static bool set_pulse_replacements( long pnum, long num_repl, 
+									long *repl_list );
+
+static bool get_pulse_function( long pnum, int *function );
+static bool get_pulse_position( long pnum, double *time );
+static bool get_pulse_length( long pnum, double *time );
+static bool get_pulse_position_change( long pnum, double *time );
+static bool get_pulse_length_change( long pnum, double *time );
+static bool get_pulse_maxlen( long pnum, double *time );
+
+static Ticks double2ticks( double time );
+static double ticks2double( Ticks ticks );
+static void check_pod_level_diff( double high, double low );
+PULSE *get_pulse( long pnum );
+
