3eb6634ffdf681cbb490e67e35c881dbd9be6342
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-02-10 17:23:05 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@822 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/global.h
+++ trunk/fsc2/src/global.h
@@ -21,6 +21,16 @@
 
 #define DEFAULT_PHASE_SWITCH_DELAY 2e-8    /* 20 ns */
 
+/* This defines the amount of time that we try to prolong all pulse sequences
+   to allow for a jitter due to different cable lengths with the resulting
+   problems for phase pulses (they may end a bit to early and thus lead to
+   strange artefacts at the end of an experiment) etc. If the pulser time base
+   is larger we try to allow for at least one time base at the end of the
+   pulse sequence. Define GRACE_PERIOD as 0 to switch off setting of a grace
+   period. */
+
+#define GRACE_PERIOD 1e-7                  /* 100 ns */
+
 
 /* Define number of colors to be used in 2D graphics - must be less than
    FL_MAX_COLORS - FL_FREE_COL1 + 3, i.e. not more than about 1005. On
