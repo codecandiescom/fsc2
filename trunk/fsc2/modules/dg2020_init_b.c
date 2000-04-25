2f821555293c618b644983fb69f309841b9ba689
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-04-25 23:26:57 +0000

    Initial revision
    git-svn-id: file:///usr/local/svn/fsc2@1020 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/modules/dg2020_init_b.c
@@ -0,0 +1,19 @@
+/*
+  $Id$
+*/
+
+
+
+#include "dg2020_f.h"
+
+
+/*---------------------------------------------------------------------------
+  Function does everything that needs to be done for checking and completing
+  the internal representation of the pulser at the start of a test run.
+---------------------------------------------------------------------------*/
+
+void dg2020_init_setup( void )
+{
+	/* Check that all functions that have more than one pod assigned to it
+	   also need phase cycling (and the other way round) */
+}
