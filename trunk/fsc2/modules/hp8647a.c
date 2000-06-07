b6c04b08842787a7e4268bea5ee1161d4ce08538
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-06-07 12:57:04 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@1296 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/hp8647a.c
+++ trunk/fsc2/modules/hp8647a.c
@@ -3,69 +3,8 @@
 */
 
 
-#include "fsc2.h"
+#include "hp8647a.h"
 #include "gpib.h"
-
-
-#define DEVICE_NAME "HP8647A"      /* compare entry in /etc/gpib.conf ! */
-
-#define MIN_FREQ  2.5e5            /* 250 kHz  */
-#define MAX_FREQ  1.0e9            /* 1000 MHz */
-#define MIN_ATTEN 10.0             /* +10 db   */
-#define MAX_ATTEN -136.0           /* -136 db  */
-
-#define DEFAULT_TABLE_FILE "hp8647a.table"     /* libdir will be prepended ! */
-
-
-
-/* declaration of exported functions */
-
-int hp8647a_init_hook( void );
-int hp8647a_test_hook( void );
-int hp8647a_end_of_test_hook( void );
-int hp8647a_exp_hook( void );
-int hp8647a_end_of_exp_hook( void );
-void hp8647a_exit_hook( void );
-
-
-Var *synthesizer_state( Var *v );
-Var *synthesizer_frequency( Var *v );
-Var *synthesizer_step_frequency( Var *v );
-Var *synthesizer_attenuation( Var *v );
-Var *synthesizer_sweep_up( Var *v );
-Var *synthesizer_sweep_down( Var *v );
-Var *synthesizer_reset_frequency( Var *v );
-Var *synthesizer_use_table( Var *v );
-Var *synthesizer_attenuation( Var *v );
-
-
-
-typedef struct {
-	double freq;
-	double att;
-} ATT_TABLE_ENTRY;
-
-typedef struct
-{
-	int device;
-
-	double freq;
-	bool freq_is_set;
-	double step_freq;
-	bool step_freq_is_set;
-	double start_freq;
-	bool start_freq_is_set;
-	double attenuation;
-	bool attenuation_is_set;
-
-	bool state;
-
-	char *table_file;      /* name of attenuation table file */
-	bool use_table;
-	ATT_TABLE_ENTRY **att_table;
-	long num_att_table_entries;
-
-} HP8647A;
 
 
 static HP8647A hp8647a,
@@ -864,6 +803,7 @@ static bool hp8647a_read_table( FILE *fp )
 {
 	/* Lots of stuff to come... */
 
+	hp8647a_reader( fp, DEVICE_NAME );
 	fclose( fp );
 	return OK;
 }
