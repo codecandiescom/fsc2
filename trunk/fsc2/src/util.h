73504501b3b0669c6b850c8f4fe3b6a079af5bca
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-06-09 19:15:53 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@1319 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/util.h
+++ trunk/fsc2/src/util.h
@@ -16,6 +16,7 @@ long get_file_length( char *name, int *len );
 void eprint( int severity, const char *fmt, ... );
 bool fsc2_locking( void );
 void delete_stale_shms( void );
+int is_in( const char *supplied_in, const char **altern, int max );
 void i2rgb( double h, int *rgb );
 void create_colors( void );
 inline unsigned long d2color( double a );
