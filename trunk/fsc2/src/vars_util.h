e4d7ea75a0411919e6759021cae9c52fdf39b473
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-06-18 01:37:29 +0000

    Initial revision
    git-svn-id: file:///usr/local/svn/fsc2@1375 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/src/vars_util.h
@@ -0,0 +1,19 @@
+/*
+  $Id$
+*/
+
+
+#if ! defined VARS_UTIL_HEADER
+#define VARS_UTIL_HEADER
+
+
+#include "fsc2.h"
+
+
+Var *vars_add_to_int_var( Var *v1, Var *v2 );
+Var *vars_add_to_float_var( Var *v1, Var *v2 );
+Var *vars_add_to_int_arr( Var *v1, Var *v2 );
+Var *vars_add_to_float_arr( Var *v1, Var *v2 );
+
+
+#endif  /* ! VARS_UTIL_HEADER */
