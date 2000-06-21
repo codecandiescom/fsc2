0a83f17a0072cdb5d9c201c182576498d228decb
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-06-21 17:16:20 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@1416 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/run.c
+++ trunk/fsc2/src/run.c
@@ -403,19 +403,43 @@ static void set_buttons_for_run( int active )
 	if ( active == 0 )
 	{
 		fl_freeze_form( main_form->fsc2 );
+
+		fl_deactivate_object( main_form->Load );
+		fl_set_object_lcol( main_form->Load, FL_INACTIVE_COL );
+
+		fl_deactivate_object( main_form->reload );
+		fl_set_object_lcol( main_form->reload, FL_INACTIVE_COL );
+
+		fl_deactivate_object( main_form->test_file );
+		fl_set_object_lcol( main_form->test_file, FL_INACTIVE_COL );
+
 		fl_deactivate_object( main_form->run );
 		fl_set_object_lcol( main_form->run, FL_INACTIVE_COL );
+
 		fl_deactivate_object( main_form->quit );
 		fl_set_object_lcol( main_form->quit, FL_INACTIVE_COL );
+
 		fl_unfreeze_form( main_form->fsc2 );
 	}
 	else
 	{
 		fl_freeze_form( main_form->fsc2 );
+
+		fl_activate_object( main_form->Load );
+		fl_set_object_lcol( main_form->Load, FL_BLACK );
+
+		fl_activate_object( main_form->reload );
+		fl_set_object_lcol( main_form->reload, FL_BLACK );
+
+		fl_activate_object( main_form->test_file );
+		fl_set_object_lcol( main_form->test_file, FL_BLACK );
+
 		fl_activate_object( main_form->run );
 		fl_set_object_lcol( main_form->run, FL_BLACK );
+
 		fl_activate_object( main_form->quit );
 		fl_set_object_lcol( main_form->quit, FL_BLACK );
+
 		fl_unfreeze_form( main_form->fsc2 );
 	}
 }
