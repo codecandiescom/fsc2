9b2a6d1ebd5fca8154f573d25166682e8e402ad9
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-09-05 18:54:16 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@1901 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/fsc2_connect.c
+++ trunk/fsc2/src/fsc2_connect.c
@@ -2,7 +2,34 @@
   $Id$
 */
 
-/* Return codes:
+/* 
+   This program expects an EDL program on standard input. It will then read it
+   in and write it into a temporary file and try to connect to fsc2 via a
+   socket on which fsc2 is supposed to listen. If this succeeds it sends fsc2
+   the UID, followed by a letter that depends on the name the program was
+   invocated with - if it was started as 'fsc2_start' it sends an 's', if as
+   'fsc2_test' it sends 't' and if invocated as either 'fsc2_load' or
+   'fsc2_connect' it sends an 'l', thus indicating what fsc2 is supposed to
+   do, i.e. either start the EDL program immediately, to test it or only to
+   load it. It also sends a second letter, 'b' to tell fsc2 to delete the
+   temporary file when it's done with it. Finally it sends the file name.
+   fsc2 can react in different ways: It can indicate that it is either run by
+   a user with a different UID, that it is busy, or that it couldn't read the
+   messages, which in turn is returned by this program via its return value
+   (see list below).
+
+   If, on the other hand, the program finds that fsc2 isn't running at all
+   (because it can't connect to fsc2) it will try to start fsc2 with the '-d'
+   flag (to tell fsc2 to delete its input file when done with it) and,
+   depending on the name this program was invocated with, with the '-S' or 'T'
+   flag or no flag and the name of the temporary file. If fsc2 couldn't be
+   started properly this program will return a return value to indicate it.
+
+   The best way to use this program is to create the EDL program to send to
+   fsc2 and send it to this program using a popen() call. The success can be
+   obtained by examining the return status of the following call of pclose().
+
+   Return codes:
 
     0: Everything ok
    -1: Internal error
@@ -18,51 +45,60 @@
 #include <unistd.h>
 #include <errno.h>
 #include <assert.h>
-#include <limits.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
+
+/* FSC2_SOCKET must be identical to the definition in fsc2.h ! */
+
 #define FSC2_SOCKET  "/tmp/fsc2.uds"
+
 #define MAXLINE      4096
 
 
-char fname[ ] = P_tmpdir "/fsc2.edl.XXXXXX";
-volatile int sig_type;
-
-
-void make_tmp_file( void );
-int open_socket( void );
-void start_fsc2( const char *pname );
+void make_tmp_file( char *fname );
+int open_socket( const char *fname );
+void start_fsc2( const char *pname, const char *fname );
 void sig_handler( int signo );
-void contact_fsc2( int sock_fd, const char *pname );
+void contact_fsc2( int sock_fd, const char *pname, const char *fname );
+void clean_up( const char *fname, int sock_fd, int ret_val );
 ssize_t writen( int fd, const void *vptr, size_t n );
 ssize_t read_line( int fd, void *vptr, size_t max_len );
 ssize_t do_read( int fd, char *ptr );
 
 
+volatile int sig_type = 0;
+
+
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+
 int main( int argc, char *argv[ ] )
 {
+	char fname[ ] = P_tmpdir "/fsc2.edl.XXXXXX";
 	int sock_fd;
 
 
 	argc = argc;
 
 
-	make_tmp_file( );
-	if ( ( sock_fd = open_socket( ) ) == -1 )
-		start_fsc2( argv[ 0 ] );
+	make_tmp_file( fname );
+	if ( ( sock_fd = open_socket( fname ) ) == -1 )
+		start_fsc2( argv[ 0 ], fname );
 	else
-		contact_fsc2( sock_fd, argv[ 0 ] );
+		contact_fsc2( sock_fd, argv[ 0 ], fname );
 
 	return 0;
 }
 
 
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
 
-void make_tmp_file( void )
+void make_tmp_file( char *fname )
 {
 	int tmp;
 	ssize_t bytes_read;
@@ -100,12 +136,14 @@ void make_tmp_file( void )
 		}
 	}
 
-	close( tmp );	
+	close( tmp );
 }
 
 
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
 
-int open_socket( void )
+int open_socket( const char *fname )
 {
 	int sock_fd;
 	struct sockaddr_un serv_addr;
@@ -124,28 +162,31 @@ int open_socket( void )
 	strcpy( serv_addr.sun_path, FSC2_SOCKET );
 
 	/* If connect fails due to connection refused or because there's no socket
-	   file (or it it isn't a socket file at all) that means fsc2 isn't
-	   running and we've got to start it */
+	   file (or it exists but isn't a socket file) it means fsc2 isn't running
+	   and we've got to start it */
 
 	if ( connect( sock_fd, (struct sockaddr_un * ) &serv_addr,
 				  sizeof( serv_addr ) ) == -1 )
 	{
-		if ( errno == ECONNREFUSED || errno == ENOTSOCK || errno == ENOENT )
+		if ( errno == ECONNREFUSED || errno == ENOENT || errno == ENOTSOCK )
 		{
 			close( sock_fd );
 			return -1;
 		}
 
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
+		/* Bomb out on other errors */
+
+		clean_up( fname, sock_fd, -1 );
 	}
 
 	return sock_fd;
 }
 
 
-void start_fsc2( const char *pname )
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+
+void start_fsc2( const char *pname, const char *fname )
 {
 	char *av[ 6 ] = { NULL, NULL, NULL, NULL, NULL, NULL };
 	int ac = 0;
@@ -186,43 +227,56 @@ void start_fsc2( const char *pname )
 		exit( -1 );
 	}
 
-	av[ ac++ ] = fname;
+	av[ ac++ ] = ( char * ) fname;
 
 	signal( SIGUSR1, sig_handler );
 	signal( SIGCHLD, sig_handler );
 
 	if ( ( new_pid = fork( ) ) == 0 )
+	{
 		execvp( av[ 0 ], av );
+		_exit( -1 );                 /* kill child on failed execvp() */
+	}
 
 	free( av[ 0 ] );
 
-	if ( new_pid < 0 )          /* fork failed ? */
+	if ( new_pid < 0 )               /* fork failed ? */
 	{
 		unlink( fname );
 		exit( -1 ) ;
 	}
 
-	sleep( INT_MAX );           /* wait for fsc2 to send signal or fail */
-
-	if ( sig_type == SIGCHLD )
+	while ( ! sig_type )
 	{
-		unlink( fname );
-		exit( 4 );
-	}
+		pause( );                    /* wait for fsc2 to send signal or fail */
 
-	if ( sig_type != SIGUSR1 )
-		exit( -1 );
+		if ( sig_type == SIGCHLD )   /* fsc2 failed to start */
+		{
+			unlink( fname );
+			exit( 4 );
+		}
+	}
 }
 
 
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+
 void sig_handler( int signo )
 {
-	assert( signo == SIGUSR1 || signo == SIGCHLD );
+	/* Bomb out on unexpected signals */
+
+	if ( signo != SIGCHLD && signo != SIGUSR1 )
+		exit( -1 );
+
 	sig_type = signo;
 }
 
 
-void contact_fsc2( int sock_fd, const char *pname )
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+
+void contact_fsc2( int sock_fd, const char *pname, const char *fname )
 {
 	char line[ MAXLINE ];
 	ssize_t count;
@@ -234,39 +288,19 @@ void contact_fsc2( int sock_fd, const char *pname )
 	sprintf( line, "%d\n", getuid( ) );
 	if ( writen( sock_fd, line, strlen( line ) )
 		 != ( ssize_t ) strlen( line ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	if ( ( count = read_line( sock_fd, line, MAXLINE ) ) <= 0 )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	if ( ! strcmp( line, "FAIL\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( 1 );
-	}
+		clean_up( fname, sock_fd, 1 );
 
 	if ( ! strcmp( line, "BUSY\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( 2 );
-	}
+		clean_up( fname, sock_fd, 2 );
 
 	if ( strcmp( line, "OK\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	/* Assemble second string to send, the first character is the method ('s'
 	   for start, 't' for test or 'l' for load). The method is deduced from
@@ -279,6 +313,7 @@ void contact_fsc2( int sock_fd, const char *pname )
 	else
 		prog_name = ( char * ) pname;
 	strcpy( line, " d\n" );
+
 	if ( ! strcmp( prog_name, "fsc2_start" ) )
 		line[ 0 ] = 's';
 	if ( ! strcmp( prog_name, "fsc2_test" ) )
@@ -288,43 +323,23 @@ void contact_fsc2( int sock_fd, const char *pname )
 		line[ 0 ] = 'l';
 
 	if ( line[ 0 ] == ' ' )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	/* Now tell fsc2 the method - it can either reply wtih "BUSY\n" or with
        "OK\n" */
 
 	if ( writen( sock_fd, line, strlen( line ) )
 		 != ( ssize_t ) strlen( line ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	if ( ( count = read_line( sock_fd, line, MAXLINE ) ) <= 0 )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	if ( ! strcmp( line, "BUSY\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( 2 );
-	}
+		clean_up( fname, sock_fd, 2 );
 
 	if ( strcmp( line, "OK\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	/* Finally we tell fsc2 the name of the temporary file */
 
@@ -332,43 +347,37 @@ void contact_fsc2( int sock_fd, const char *pname )
 	strcat( line, "\n" );
 	if ( writen( sock_fd, line, strlen( line ) )
 		 != ( ssize_t ) strlen( line ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	if ( ( count = read_line( sock_fd, line, MAXLINE ) ) <= 0 )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	if ( ! strcmp( line, "FAIL\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( 3 );
-	}
+		clean_up( fname, sock_fd, 3 );
 
 	if ( ! strcmp( line, "BUSY\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( 2 );
-	}
+		clean_up( fname, sock_fd, 2 );
 
 	if ( strcmp( line, "OK\n" ) )
-	{
-		unlink( fname );
-		close( sock_fd );
-		exit( -1 );
-	}
+		clean_up( fname, sock_fd, -1 );
 
 	close( sock_fd );
 }
 
+
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+
+void clean_up( const char *fname, int sock_fd, int ret_val )
+{
+	unlink( fname );
+	close( sock_fd );
+	exit( ret_val );
+}
+
+
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
 
 ssize_t read_line( int fd, void *vptr, size_t max_len )
 {
@@ -400,6 +409,9 @@ ssize_t read_line( int fd, void *vptr, size_t max_len )
 }
 
 
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+
 ssize_t do_read( int fd, char *ptr )
 {
 	static int read_cnt;
@@ -427,6 +439,9 @@ ssize_t do_read( int fd, char *ptr )
 	return 1;
 }
 
+
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
 
 ssize_t writen( int fd, const void *vptr, size_t n )
 {
