505105ff05547c940c78f2a26b2d2fd3e4c78ba2
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-09-21 18:45:20 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@1964 934aee4a-8510-0410-ae50-ec5b2ea4bb86

---
+++ trunk/fsc2/modules/hfs9000.c
@@ -0,0 +1,121 @@
+/*
+  $Id$
+*/
+
+
+#define HFS9000_MAIN
+
+#include "hfs9000.h"
+#include "gpib_if.h"
+
+
+
+/*---------------------------------------------------------------------------
+  This function is called directly after all modules are loaded. Its function
+  is to initialize all global variables that are needed in the module.
+---------------------------------------------------------------------------*/
+
+int dg2020_b_init_hook( void )
+{
+	int i;
+
+
+	/* Now test that the name entry in the pulser structure is NULL, otherwise
+	   assume, that another pulser driver has already been installed. */
+
+	if ( pulser_struct.name != NULL )
+	{
+		eprint( FATAL, "%s:%ld: While loading driver HFS9000 found that "
+				"driver %s is already installed.\n",
+				Fname, Lc, pulser_struct.name );
+		THROW( EXCEPTION );
+	}
+
+	pulser_struct.name = get_string_copy( "HFS9000" );
+
+	/* Set global variable to indicate that GPIB bus is needed */
+
+	need_GPIB = SET;
+
+	/* We have to set up the global structure for the pulser, especially the
+	   pointers for the functions that will get called from pulser.c */
+
+	hfs9000.needs_update = UNSET;
+	hfs9000.is_running = UNSET;
+
+	pulser_struct.set_timebase = hfs9000_store_timebase;
+
+	pulser_struct.assign_function = hfs9000_assign_function;
+	pulser_struct.assign_channel_to_function = NULL;
+	pulser_struct.invert_function = hfs9000_invert_function;
+	pulser_struct.set_function_delay = hfs9000_set_function_delay;
+	pulser_struct.set_function_high_level = hfs9000_set_function_high_level;
+	pulser_struct.set_function_low_level = hfs9000_set_function_low_level;
+
+	pulser_struct.set_trigger_mode = hfs9000_set_trigger_mode;
+	pulser_struct.set_repeat_time = hfs9000_set_repeat_time;
+	pulser_struct.set_trig_in_level = hfs9000_set_trig_in_level;
+	pulser_struct.set_trig_in_slope = hfs9000_set_trig_in_slope;
+	pulser_struct.set_trig_in_impedance = hfs9000_set_trig_in_impedance;
+
+	pulser_struct.set_phase_reference = NULL;
+
+	pulser_struct.new_pulse = hfs9000_new_pulse;
+	pulser_struct.set_pulse_function = hfs9000_set_pulse_function;
+	pulser_struct.set_pulse_position = hfs9000_set_pulse_position;
+	pulser_struct.set_pulse_length = hfs9000_set_pulse_length;
+	pulser_struct.set_pulse_position_change =
+		hfs9000_set_pulse_position_change;
+	pulser_struct.set_pulse_length_change = hfs9000_set_pulse_length_change;
+	pulser_struct.set_pulse_phase_cycle = NULL;
+	pulser_struct.set_grace_period = NULL;
+
+	pulser_struct.get_pulse_function = hfs9000_get_pulse_function;
+	pulser_struct.get_pulse_position = hfs9000_get_pulse_position;
+	pulser_struct.get_pulse_length = hfs9000_get_pulse_length;
+	pulser_struct.get_pulse_position_change =
+		hfs9000_get_pulse_position_change;
+	pulser_struct.get_pulse_length_change = hfs9000_get_pulse_length_change;
+	pulser_struct.get_pulse_phase_cycle = NULL;
+
+	pulser_struct.phase_setup_prep = NULL;
+	pulser_struct.phase_setup = NULL;
+
+	pulser_struct.set_phase_switch_delay = NULL;
+
+	/* Finally, we initialize variables that store the state of the pulser */
+
+	hfs9000.is_timebase = UNSET;
+
+	hfs9000.is_trig_in_mode = UNSET;
+	hfs9000.is_trig_in_slope = UNSET;
+	hfs9000.is_trig_in_level = UNSET;
+	hfs9000.is_repeat_time = UNSET;
+	hfs9000.is_neg_delay = UNSET;
+	hfs9000.neg_delay = 0;
+
+	for ( i = 0; i < MAX_CHANNELS; i++ )
+	{
+		hfs9000.channel[ i ].self = i;
+		hfs9000.channel[ i ].function = NULL;
+	}
+
+	for ( i = 0; i < PULSER_CHANNEL_NUM_FUNC; i++ )
+	{
+		hfs9000.function[ i ].self = PULSER_CHANNEL_FUNC_MIN + i;
+		hfs9000.function[ i ].is_used = UNSET;
+		hfs9000.function[ i ].is_needed = UNSET;
+		hfs9000.function[ i ].channel = NULL;
+		hfs9000.function[ i ].num_pulses = 0;
+		hfs9000.function[ i ].pulses = NULL;
+		hfs9000.function[ i ].is_inverted = UNSET;
+		hfs9000.function[ i ].delay = 0;
+		hfs9000.function[ i ].is_delay = UNSET;
+		hfs9000.function[ i ].is_high_level = UNSET;
+		hfs9000.function[ i ].is_low_level = UNSET;
+	}
+
+	hfs9000_is_needed = SET;
+
+	return 1;
+}
---
+++ trunk/fsc2/modules/hfs9000.h
@@ -0,0 +1,200 @@
+/*
+  $Id$
+*/
+
+
+#include "fsc2.h"
+
+
+/* Here are all the directly exported functions (i.e. exported either implicit
+   as a hook functions or via the Functions data base) */
+
+int hfs9000_init_hook( void );
+int hfs9000_test_hook( void );
+int hfs9000_end_of_test_hook( void );
+int hfs9000_exp_hook( void );
+int hfs9000_end_of_exp_hook( void );
+void hfs9000_exit_hook( void );
+
+
+Var *pulser_update( Var *v );
+Var *pulser_shift( Var *v );
+Var *pulser_increment( Var *v );
+Var *pulser_pulse_reset( Var *v );
+Var *pulser_lock_keyboard( Var *v );
+
+
+/* Definitions needed for the pulser */
+
+#define Ticks long              // for times in units of the pulsers time base
+#define Ticks_max l_max
+#define Ticks_min l_min
+
+
+/* name of device as given in GPIB configuration file /etc/gpib.conf */
+
+#define DEVICE_NAME "HFS9000"
+
+
+#define MIN_TIMEBASE            1.6e-9     // minimum pulser time base: 1.6 ns
+#define MAX_TIMEBASE            2.0e-5     // maximum pulser time base: 20 us
+
+#define MAX_CHANNELS            4          // number of channels
+
+
+# define MIN_POD_HIGH_VOLTAGE  -1.5
+# define MAX_POD_HIGH_VOLTAGE   5.5
+
+# define MIN_POD_LOW_VOLTAGE   -2.0
+# define MAX_POD_LOW_VOLTAGE    5.0
+
+#define MAX_POD_VOLTAGE_SWING   5.5
+#define MIN_POD_VOLTAGE_SWING   0.5
+
+#define VOLTAGE_RESOLUTION      0.01
+
+#define MAX_TRIG_IN_LEVEL       4.7
+#define MIN_TRIG_IN_LEVEL      -4.7
+
+#define MAX_PULSER_BITS    65536  // maximum number of bits in channel
+
+#define START ( ( bool ) 1 )
+#define STOP  ( ( bool ) 0 )
+
+
+typedef struct _F_ {
+	int self;                    // the functions number
+	bool is_used;                // set if the function has been declared in
+	                             // the ASSIGNMENTS section
+	bool is_needed;              // set if the function has been assigned
+                                 // pulses
+	struct _C_ *channel;         // channel assigned to function
+	bool need_constant;
+
+	int num_pulses;              // number of pulses assigned to the function
+	int num_active_pulses;       // number of pulses currenty in use
+	struct _p_ **pulses;         // list of pulse pointers
+
+	long max_seq_len;            // maximum length of the pulse sequence
+
+	bool is_inverted;            // if set polarity is inverted
+
+	Ticks delay;                 // delay for the function/channel combination
+	bool is_delay;
+
+	double high_level;           // high and low voltage levels for the pod(s)
+	double low_level;            // associated with the fucntion
+
+	bool is_high_level;
+	bool is_low_level;
+
+} FUNCTION;
+
+typedef struct _C_ {
+	int self;
+	FUNCTION *function;
+	bool needs_update;
+} CHANNEL;
+
+typedef struct {
+	int device;              // GPIB number of the device
+
+	double timebase;         // time base of the digitizer
+	bool is_timebase;
+
+	int trig_in_mode;        //	EXTERNAL or INTERNAL
+	int trig_in_slope;       //	only in EXTERNAL mode
+	double trig_in_level;    //	only in EXTERNAL mode
+	int trig_in_impedance;   //	only in EXTERNAL mode
+	Ticks repeat_time;       //	only in INTERNAL mode
+
+	bool is_cw_mode;         // set for cw mode
+
+	bool is_trig_in_mode;
+	bool is_trig_in_slope;
+	bool is_trig_in_level;
+	bool is_trig_in_impedance;
+	bool is_repeat_time;
+
+	Ticks neg_delay;
+	bool is_neg_delay;       // if any of the functions has a negative delay
+
+	long max_seq_len;        // maximum length of all pulse sequences
+
+	FUNCTION function[ PULSER_CHANNEL_NUM_FUNC ];
+	CHANNEL channel[ MAX_CHANNELS ];
+
+	int needed_channels;     // number of channels that are going to be needed
+	                         // in the experiment
+
+	bool needs_update;       // set if pulse properties have been changed in
+                             // test run or experiment
+	bool is_running;         // set if the pulser is in run mode
+
+	Ticks mem_size;          // size of the complete sequence, i.e. including
+	                         // the memory needed for padding
+} HFS9000;
+
+
+typedef struct _p_ {
+
+	long num;                // (positive) number of the pulse 
+
+	bool is_active;          // set if the pulse is really used
+	bool was_active;
+	bool has_been_active;    // used to find useless pulses
+
+	struct _p_ *next;
+	struct _p_ *prev;
+
+	FUNCTION *function;      // function the pulse is associated with
+
+	Ticks pos;               // current position, length, position change and
+	Ticks len;               // length change of pulse (in units of the pulsers
+	Ticks dpos;              // time base)
+	Ticks dlen;
+
+	bool is_function;        // flags that are set when the corresponding
+	bool is_pos;             // property has been set
+	bool is_len;
+	bool is_dpos;
+	bool is_dlen;
+
+	Ticks initial_pos;       // position, length, position change and length
+	Ticks initial_len;       // change at the start of the experiment
+	Ticks initial_dpos;
+	Ticks initial_dlen;
+
+	bool initial_is_pos;     // property has initially been set
+	bool initial_is_len;
+	bool initial_is_dpos;
+	bool initial_is_dlen;
+
+	Ticks old_pos;           // position and length of pulse before a change
+	Ticks old_len;           // is applied
+
+	bool is_old_pos;
+	bool is_old_len;
+
+	CHANNEL *channel;
+
+	bool needs_update;       // set if the pulses properties have been changed
+                             // in test run or experiment
+} PULSE;
+
+
+#if defined( HFS9000_MAIN )
+
+bool hfs9000_is_needed = UNSET;
+HFS9000 hfs9000;
+PULSE *hfs9000_Pulses = NULL;
+bool hfs9000_IN_SETUP = UNSET;
+
+#else
+
+extern bool hfs9000_is_needed;
+extern HFS9000 hfs9000;
+extern PULSE *hfs9000_Pulses;
+extern bool hfs9000_IN_SETUP;
+
+#endif
