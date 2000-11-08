8ad739f419ef721cb61fe139332798c7fecbd4a5
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2000-11-08 22:47:34 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@2120 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/graph_cut.h
+++ trunk/fsc2/src/graph_cut.h
@@ -6,6 +6,36 @@
 #define GRAPH_CUT_HEADER
 
 
+typedef struct {
+	int cut_dir;            /* direction of cut (i.e. X, Y) */
+
+	bool is_scale_set;      /* have scaling factors been calculated ? */
+	bool scale_changed;     /* have scaling factors changed ? */
+	bool is_fs;             /* state of full scale button */
+
+	int cur_1,
+		cur_2,
+		cur_3,
+		cur_4,
+		cur_5,
+		cur_6,
+		cur_7,
+	    cur_8;
+
+	Pixmap label_pm[ 3 ];   /* used for drawing of rotated text */
+
+	unsigned int up_arrow_w,      /* sizes of out of range markers */
+		         up_arrow_h,
+		         down_arrow_w,
+		         down_arrow_h,
+		         left_arrow_w,
+		         left_arrow_h,
+		         right_arrow_w,
+		         right_arrow_h;
+
+} Cut_Graphics;
+
+
 #include "fsc2.h"
 
 void cut_show( int dir, int pos );
