6d4c083edd697b07cfc4508e7e85d69c5a9d858d
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-02-18 15:13:02 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@2461 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/hfs9000.c
+++ trunk/fsc2/modules/hfs9000.c
@@ -56,7 +56,7 @@ int hfs9000_init_hook( void )
 	pulser_struct.set_repeat_time = NULL;
 	pulser_struct.set_trig_in_level = hfs9000_set_trig_in_level;
 	pulser_struct.set_trig_in_slope = hfs9000_set_trig_in_slope;
-	pulser_struct.set_trig_in_impedance = NULL;    /*!!!!!!!!!!!!!!!!!!!*/
+	pulser_struct.set_trig_in_impedance = NULL;
 
 	pulser_struct.set_phase_reference = NULL;
 
