bcc15fa13050070dac9b7ed9749c46401391e564
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-03-14 11:20:29 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@2617 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/func.h
+++ trunk/fsc2/src/func.h
@@ -15,6 +15,8 @@ typedef struct
 	long nargs;                       /* number of arguments */
     int access_flag;                  /* asscessibility flag */
 	bool to_be_loaded;                /* set if function has to be loaded */
+	Device *device;                   /* handle of defining device module */
+	int count;
 } Func;
 
 
