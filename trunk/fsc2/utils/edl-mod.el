128ee4e8f0f89ff5c6b8cc70272647a14537487f
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-05-16 17:23:15 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@2853 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/utils/edl-mod.el
+++ trunk/fsc2/utils/edl-mod.el
@@ -1,26 +1,28 @@
 ;;; edl-mod.el --- editing EDL files with Emacs
-
+;;
+;; $Id$
+;;
 ;; Copyright (C) 2000 Jens Thoms Törring
-
+;;
 ;; Author: Jens Thoms Törring <jens@masklin.anorg.chemie.uni-frankfurt.de>
 ;; Maintainer: Jens Thoms Törring <jens@masklin.anorg.chemie.uni-frankfurt.de>
 ;; Keywords: languages
-
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;;
+;; This file is part of fsc2.
+;;
+;; Fsc2 is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
+;;
+;; Fsc2 is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; along with fsc2; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
