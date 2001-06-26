8825b3f229a79a641b656544b1051d44281f15c9
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-06-26 08:54:05 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@3144 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/util.h
+++ trunk/fsc2/src/util.h
@@ -54,9 +54,9 @@ inline double d_max( double a, double b );
 inline double d_min( double a, double b );
 
 
-/* Needed for glib versions below 2.0 (or 2.1 ?) */
+/* Needed for glib versions below 2.0 */
 
-#if ! defined ( lround )
+#if defined IS_STILL_LIBC1
 inline long lround( double x );
 #endif
 
