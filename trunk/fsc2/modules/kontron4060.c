92261272b2ac8eab3a57640a1016d563aa277d7a
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-06-30 00:52:55 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@3170 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/kontron4060.c
+++ trunk/fsc2/modules/kontron4060.c
@@ -200,8 +200,6 @@ Var *voltmeter_get_data( Var *v )
 
 static bool kontron4060_init( const char *name )
 {
-	fsc2_assert( kontron4060.device < 0 );
-
 	if ( gpib_init_device( name, &kontron4060.device ) == FAILURE )
         return FAIL;
 
