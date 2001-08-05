9b023aa91231fae729cb14084456a1ccc8978560
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-08-05 18:03:37 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@3383 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/fsc2.c
+++ trunk/fsc2/src/fsc2.c
@@ -1311,8 +1311,6 @@ void main_sig_handler( int signo )
 	switch ( signo )
 	{
 		case SIGCHLD :
-			fprintf( stderr, "Child died\n" );
-			fflush( stderr );
 			errno_saved = errno;
 			wait( NULL );
 			errno = errno_saved;
