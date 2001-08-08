53c16694b57209a53e2f94eb944ca0092cd9ff51
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2001-08-08 11:22:48 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@3430 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/User_Functions.c
+++ trunk/fsc2/modules/User_Functions.c
@@ -89,7 +89,7 @@ static Var *get_phase_cycled_area_1( Var *v )
 	int access;
 	double *data;
 	int channels_needed = 1;
-	volatile Acquisition_Sequence *aseq;
+	static Acquisition_Sequence *aseq;
 
 
 	V = v;
