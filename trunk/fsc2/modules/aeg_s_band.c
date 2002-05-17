73b9f18de6a7946a0c63d1557af24dd36ef74a66
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2002-05-17 16:19:59 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@4561 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/aeg_s_band.c
+++ trunk/fsc2/modules/aeg_s_band.c
@@ -532,7 +532,7 @@ static double aeg_s_band_field_check( double field, bool *err_flag )
 		if ( field > AEG_S_BAND_WITH_ER035M_MAX_FIELD )
 		{
 			print( FATAL, "Field (%lf G) too high for Bruker ER035M "
-				   "gaussmeter, maximum is %d G.\n",
+				   "gaussmeter, maximum is %d G.\n", field,
 				   ( int ) AEG_S_BAND_WITH_ER035M_MAX_FIELD );
 			if ( FSC2_MODE == EXPERIMENT )
 			{
