c62c652157b3ecad8bd8cd2c01976a94baf12c94
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2002-09-13 10:18:28 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@5087 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/func_intact_b.c
+++ trunk/fsc2/src/func_intact_b.c
@@ -235,6 +235,7 @@ Var *f_bcreate( Var *var )
 	new_io->help_text = help_text;
 	new_io->partner = coll;
 	new_io->is_changed = UNSET;
+	new_io->report_change = UNSET;
 
 	/* Draw the new button */
 
