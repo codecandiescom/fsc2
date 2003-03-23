18b17eb9a212e6a46b1810d55c92967a6763fdf3
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2003-03-23 14:50:19 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@5838 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/serial.h
+++ trunk/fsc2/src/serial.h
@@ -35,7 +35,7 @@ struct termios *fsc2_serial_open( int sn, const char *devname, int flags );
 void fsc2_serial_close( int sn );
 ssize_t fsc2_serial_write( int sn, const void *buf, size_t count );
 ssize_t fsc2_serial_read( int sn, void *buf, size_t count,
-						  long us_wait, bool, quit_on_signal );
+						  long us_wait, bool quit_on_signal );
 int fsc2_tcgetattr( int sn, struct termios *termios_p );
 int fsc2_tcsetattr( int sn, int optional_actions, struct termios *termios_p );
 int fsc2_tcsendbreak( int sn, int duration );
