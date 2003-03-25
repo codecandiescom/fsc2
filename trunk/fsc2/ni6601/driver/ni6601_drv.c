a8987e2f0e3db5f6cb91e25c7459e9f69aca2fd9
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2003-03-25 14:07:56 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@5851 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/ni6601/driver/ni6601_drv.c
+++ trunk/fsc2/ni6601/driver/ni6601_drv.c
@@ -10,13 +10,13 @@
   the Free Software Foundation; either version 2, or (at your option)
   any later version.
 
-  Fsc2 is distributed in the hope that it will be useful,
+  This driver is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with fsc2; see the file COPYING.  If not, write to
+  along with this driver; see the file COPYING.  If not, write to
   the Free Software Foundation, 59 Temple Place - Suite 330,
   Boston, MA 02111-1307, USA.
 */
