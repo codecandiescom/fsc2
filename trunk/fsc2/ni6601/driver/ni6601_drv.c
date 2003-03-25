d18d93e66540f038b6800cbf1d3160cddf55b674
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2003-03-25 14:10:15 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@5852 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/ni6601/driver/ni6601_drv.c
+++ trunk/fsc2/ni6601/driver/ni6601_drv.c
@@ -10,13 +10,13 @@
   the Free Software Foundation; either version 2, or (at your option)
   any later version.
 
-  This driver is distributed in the hope that it will be useful,
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this driver; see the file COPYING.  If not, write to
+  along with this program; see the file COPYING.  If not, write to
   the Free Software Foundation, 59 Temple Place - Suite 330,
   Boston, MA 02111-1307, USA.
 */
