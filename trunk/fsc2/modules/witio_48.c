99cf3a64ebe14e6140f5f3a2fcfefe400f9a4059
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2003-04-03 21:09:23 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@5937 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/witio_48.c
+++ trunk/fsc2/modules/witio_48.c
@@ -50,6 +50,7 @@ void witio_48_exit_hook( void );
 
 
 Var *dio_name( Var *v );
+Var *dio_reserve_dio( Var *v );
 Var *dio_mode( Var *v );
 Var *dio_value( Var *v );
 
@@ -60,36 +61,54 @@ static void check_ret( int ret_val );
 
 typedef struct {
 	WITIO_48_MODE mode[ 2 ];
+	char *reserved_by[ 2 ];
 } WITIO_48;
 
 static WITIO_48 witio_48, witio_48_saved;
 
 
-/*---------------------------------------------------------*/
-/*---------------------------------------------------------*/
+/*------------------------------------------------------*/
+/* Function that gets called when the module is loaded. */
+/*------------------------------------------------------*/
 
 int witio_48_init_hook( void )
 {
 	int i;
 
+
 	for ( i = 0; i < 2; i++ )
+	{
 		witio_48.mode[ i ] = WITIO_48_MODE_1x24;
+		witio_48.reserved_by[ i ] = NULL;
+	}
 
 	return 1;
 }
 
 
-/*---------------------------------------------------------*/
-/*---------------------------------------------------------*/
+/*--------------------------------------------------------*/
+/* Function that gets called at the start of the test run */
+/*--------------------------------------------------------*/
 
 int witio_48_test_hook( void )
 {
-	witio_48_saved = witio_48;
+	int i;
+
+
+	for ( i = 0; i < 2; i++ )
+	{
+		witio_48_saved.mode[ i ] = witio_48.mode[ i ];
+		if ( witio_48.reserved_by[ i ] )
+			witio_48_saved.reserved_by[ i ] =
+										 T_strdup( witio_48.reserved_by[ i ] );
+	}
+
 	return 1;
 }
 
 
 /*---------------------------------------------------------*/
+/* Function that gets called at the start of an experiment */
 /*---------------------------------------------------------*/
 
 int witio_48_exp_hook( void )
@@ -97,18 +116,26 @@ int witio_48_exp_hook( void )
 	int i;
 
 
-	witio_48 = witio_48_saved;
-
 	for ( i = 0; i < 2; i++ )
+	{
 		check_ret( witio_48_set_mode( ( WITIO_48_DIO ) i,
 									  witio_48.mode[ i ] ) );
+
+		if ( witio_48.reserved_by[ i ] )
+			witio_48.reserved_by[ i ] =
+									CHAR_P T_free( witio_48.reserved_by[ i ] );
+		if ( witio_48_saved.reserved_by[ i ] )
+			witio_48.reserved_by[ i ] =
+								   T_strdup( witio_48_saved.reserved_by[ i ] );
+	}
 
 	return 1;
 }
 
 
-/*---------------------------------------------------------------*/
-/*---------------------------------------------------------------*/
+/*-------------------------------------------------------*/
+/* Function that gets called at the end of an experiment */
+/*-------------------------------------------------------*/
 
 int witio_48_end_of_exp_hook( void )
 {
@@ -117,11 +144,25 @@ int witio_48_end_of_exp_hook( void )
 }
 
 
-/*---------------------------------------------------------------*/
-/*---------------------------------------------------------------*/
+/*--------------------------------------------------------------*/
+/* Function that gets called just before the module is unloaded */
+/*--------------------------------------------------------------*/
 
 void witio_48_exit_hook( void )
 {
+	int i;
+
+
+	/* Get rid of the strings that might have been used for locking */
+
+	for ( i = 0; i < 2; i++ )
+	{
+		if ( witio_48.reserved_by[ i ] )
+			T_free( witio_48.reserved_by[ i ] );
+		if ( witio_48_saved.reserved_by[ i ] )
+			T_free( witio_48_saved.reserved_by[ i ] );
+	}
+
 	/* This shouldn't be necessary, I just want to make 100% sure that
 	   the device file for the board is really closed */
 
@@ -129,8 +170,9 @@ void witio_48_exit_hook( void )
 }
 
 
-/*---------------------------------------------------------------*/
-/*---------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/* Function returns a string variable with the name of the device */
+/*----------------------------------------------------------------*/
 
 Var *dio_name( Var *v )
 {
@@ -139,18 +181,31 @@ Var *dio_name( Var *v )
 }
 
 
-/*---------------------------------------------------------------*/
-/*---------------------------------------------------------------*/
+/*-----------------------------------------------------------*/
+/* The module allows to get locks on the DIOs. This is done */
+/* so that another module can reserve a DIO for its own use  */
+/* and the user can't accidentally mess around with the DIO. */
+/* To get a lock this function must be called with the DIO   */
+/* number and a pass-phrase (usually the device name for the */
+/* module requesting the lock). If there's also a third      */
+/* argument it must be boolean and if set the function will  */
+/* look the DIO, otherwise it is unlocked.                   */
+/* The function never throws an exception, it just returns 1 */
+/* on success (the requested DIO is now locked or unlocked)  */
+/* or 0 if the lock state could not be changed. Trying to    */
+/* lock twice for with the same pass-phrase succeeds but is  */
+/* basically a NOP.                                          */
+/*------------------------------------------------------------*/
 
-Var *dio_mode( Var *v )
+Var *dio_reserve_dio( Var *v )
 {
 	long dio;
-	long mode;
+	bool lock_state = SET;
 
 
 	if ( v == NULL )
 	{
-		print( FATAL, "Missing DIO argument" );
+		print( FATAL, "Missing argument(s).\n" );
 		THROW( EXCEPTION );
 	}
 
@@ -162,6 +217,104 @@ Var *dio_mode( Var *v )
 			   "(or DIO_1 or DIO_2).\n", dio + 1 );
 		THROW( EXCEPTION );
 	}
+
+	if ( ( v = vars_pop( v ) ) == NULL )
+		return vars_push( INT_VAR, witio_48.reserved_by[ dio ] ? 1L : 0L );
+
+	if ( v->type != STR_VAR )
+	{
+		print( FATAL, "Second argument isn't a string.\n" );
+		THROW( EXCEPTION );
+	}
+
+	if ( v->next != NULL )
+	{
+		lock_state = get_boolean( v->next );
+		too_many_arguments( v->next );
+	}
+	else
+		too_many_arguments( v );
+
+	if ( witio_48.reserved_by[ dio ] )
+	{
+		if ( lock_state == SET )
+		{
+			if ( ! strcmp( witio_48.reserved_by[ dio ], v->val.sptr ) )
+				return vars_push( INT_VAR, 1L );
+			else
+				return vars_push( INT_VAR, 0L );
+		}
+		else
+		{
+			if ( ! strcmp( witio_48.reserved_by[ dio ], v->val.sptr ) )
+			{
+				witio_48.reserved_by[ dio ] =
+								  CHAR_P T_free( witio_48.reserved_by[ dio ] );
+				return vars_push( INT_VAR, 1L );
+			}
+			else
+				return vars_push( INT_VAR, 0L );
+		}
+	}
+
+	if ( ! lock_state )
+		return vars_push( INT_VAR, 1L );
+
+	witio_48.reserved_by[ dio ] = T_strdup( v->val.sptr );
+	return vars_push( INT_VAR, 1L );
+}
+
+
+/*---------------------------------------------------------------*/
+/* Function for querying or setting the mode of one of the DIOs. */
+/* Modes are either "3x8", "2x12", "1x24" or "16_8". If the DIO  */
+/* has been reserved, the first argument must be the correct     */
+/* pass-phrase agreed upon in the lock operation.                */
+/*---------------------------------------------------------------*/
+
+Var *dio_mode( Var *v )
+{
+	long dio;
+	long mode;
+	char *pass = NULL;
+
+
+	if ( v == NULL )
+	{
+		print( FATAL, "Missing argument(s).\n" );
+		THROW( EXCEPTION );
+	}
+
+	if ( v->type == STR_VAR )
+		pass = v->val.sptr;
+
+	dio = get_strict_long( v->next, "DIO number" ) - 1;
+
+	if ( dio < 0 || dio > 1 )
+	{
+		print( FATAL, "Invalid DIO number %ld, must be 1 or 2 "
+			   "(or DIO_1 or DIO_2).\n", dio + 1 );
+		THROW( EXCEPTION );
+	}
+
+	if ( witio_48.reserved_by[ dio ] )
+	{
+		if ( pass == NULL )
+		{
+			print( FATAL, "DIO_%ld is reserved, phase-phrase required.\n",
+				   dio + 1 );
+			THROW( EXCEPTION );
+		}
+		else if ( strcmp( witio_48.reserved_by[ dio ], pass ) )
+		{
+			print( FATAL, "DIO_%ld is reserved, wrong phase-phrase.\n",
+				   dio + 1 );
+			THROW( EXCEPTION );
+		}
+	}
+
+	if ( pass )
+		v = vars_pop( v );
 
 	if ( ( v = vars_pop( v ) ) == NULL )
 	{
@@ -211,8 +364,11 @@ Var *dio_mode( Var *v )
 }
 
 
-/*---------------------------------------------------------------*/
-/*---------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+/* Function reads from or writes to one of the channels of one of  */
+/* the DIOs. If the DIO has been reserved, the first argument must */
+/* be the correct pass-phrase agreed upon in the lock operation.   */
+/*-----------------------------------------------------------------*/
 
 Var *dio_value( Var *v )
 {
@@ -220,15 +376,19 @@ Var *dio_value( Var *v )
 	long ch;
 	long val;
 	unsigned long uval = 0;
+	char *pass = NULL;
 
 
 	if ( v == NULL )
 	{
-		print( FATAL, "Missing DIO argument" );
+		print( FATAL, "Missing arguments.\n" );
 		THROW( EXCEPTION );
 	}
 
-	dio = get_strict_long( v, "DIO number" ) - 1;
+	if ( v->type == STR_VAR )
+		pass = v->val.sptr;
+
+	dio = get_strict_long( v->next, "DIO number" ) - 1;
 
 	if ( dio < 0 || dio > 1 )
 	{
@@ -236,6 +396,25 @@ Var *dio_value( Var *v )
 			   "(or 1 or 2).\n", dio + 1 );
 		THROW( EXCEPTION );
 	}
+
+	if ( witio_48.reserved_by[ dio ] )
+	{
+		if ( pass == NULL )
+		{
+			print( FATAL, "DIO_%ld is reserved, phase-phrase required.\n",
+				   dio + 1 );
+			THROW( EXCEPTION );
+		}
+		else if ( strcmp( witio_48.reserved_by[ dio ], pass ) )
+		{
+			print( FATAL, "DIO_%ld is reserved, wrong phase-phrase.\n",
+				   dio + 1 );
+			THROW( EXCEPTION );
+		}
+	}
+
+	if ( pass )
+		v = vars_pop( v );
 
 	if ( ( v = vars_pop( v ) ) == NULL )
 	{
@@ -316,6 +495,7 @@ Var *dio_value( Var *v )
 
 	return vars_push( INT_VAR, ( long ) uval );
 }
+
 
 /*--------------------------------------------------------------*/
 /* The function is used to translate back and forth between the */
