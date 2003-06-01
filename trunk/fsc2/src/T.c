a9b552c8934205100e49d9ad3e64646114e94cdb
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2003-06-01 23:15:41 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@6182 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/T.c
+++ trunk/fsc2/src/T.c
@@ -29,14 +29,21 @@
 #endif
 
 
-/*---------------------------------------------------------------------*/
-/*---------------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/* Replacement for malloc(), throws an OUT_OF_MEMORY_EXCEPTION if */
+/* malloc() does not succeed, checks (as long as NDEBUG isn't     */
+/* defined) that the 'size' argument isn't zero (which would be   */
+/* a hint that there's some bug in the calling code) and, if      */
+/* FSC2_MDEBUG writes out information about the address of the    */
+/* new memory segment, the amount of memory allocated and the     */
+/* address the call came from to stderr.                          */
+/*----------------------------------------------------------------*/
 
 void *T_malloc( size_t size )
 {
 	void *mem;
 #if defined FSC2_MDEBUG
-	int *EBP;           /* assumes sizeof( int ) equals size of pointers */
+	int *EBP;             /* assumes sizeof(int) equals size of pointers */
 #endif
 
 
@@ -44,7 +51,7 @@ void *T_malloc( size_t size )
 	if ( size == 0 )
 	{
 		eprint( FATAL, UNSET,
-				"Internal error detected at %s:%d (malloc with size 0).\n",
+				"Internal error detected at %s:%d (malloc with size = 0).\n",
 				__FILE__, __LINE__ );
 		THROW( EXCEPTION );
 	}
@@ -75,22 +82,38 @@ void *T_malloc( size_t size )
 }
 
 
-/*---------------------------------------------------------------------*/
-/*---------------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+/* Replacement for calloc(), throws an OUT_OF_MEMORY_EXCEPTION if */
+/* calloc() does not succeed, checks (as long as NDEBUG isn't     */
+/* defined) that both the 'nmemb' and 'size' arguments aren't     */
+/* zero (which would be a hint that there's some bug in the       */
+/* calling code) and, if FSC2_MDEBUG writes out information about */
+/* the address of the new memory segment, the amount of memory    */
+/* allocated and the address the call came from to stderr.        */
+/*----------------------------------------------------------------*/
 
 void *T_calloc( size_t nmemb, size_t size )
 {
 	void *mem;
 #if defined FSC2_MDEBUG
-	int *EBP;           /* assumes sizeof( int ) equals size of pointers */
+	int *EBP;             /* assumes sizeof(int) equals size of pointers */
 #endif
 
 
 #ifndef NDEBUG
+	if ( nmemb == 0 )
+	{
+		eprint( FATAL, UNSET,
+				"Internal error detected at %s:%d (calloc with nmemb = 0).\n",
+				__FILE__, __LINE__ );
+		THROW( EXCEPTION );
+	}
+
+
 	if ( size == 0 )
 	{
 		eprint( FATAL, UNSET,
-				"Internal error detected at %s:%d (calloc with size 0).\n",
+				"Internal error detected at %s:%d (calloc with size  = 0).\n",
 				__FILE__, __LINE__ );
 		THROW( EXCEPTION );
 	}
@@ -121,14 +144,21 @@ void *T_calloc( size_t nmemb, size_t size )
 }
 
 
-/*--------------------------------------------------------------------*/
-/*--------------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+/* Replacement for realloc(), throws an OUT_OF_MEMORY_EXCEPTION if */
+/* realloc() does not succeed, checks (as long as NDEBUG isn't     */
+/* defined) that the 'size' argument isn't zero (which would be a  */
+/* hint that there's some bug in the calling code) and, if         */
+/* FSC2_MDEBUG writes out information about the previous and the   */
+/* new address of the memory segment, the amount of memory         */
+/* allocated and the address the call came from to stderr.         */
+/*-----------------------------------------------------------------*/
 
 void *T_realloc( void *ptr, size_t size )
 {
 	void *new_ptr;
 #if defined FSC2_MDEBUG
-	int *EBP;           /* assumes sizeof( int ) equals size of pointers */
+	int *EBP;             /* assumes sizeof(int) equals size of pointers */
 #endif
 
 
@@ -167,13 +197,17 @@ void *T_realloc( void *ptr, size_t size )
 }
 
 
-/*---------------------------------------------------------------------*/
-/*---------------------------------------------------------------------*/
+/*------------------------------------------------------------------*/
+/* Replacement for free() with the difference that it returns NULL. */
+/* If FSC2_MDEBUG is defined information about the address of the   */
+/* freed memory segment and the addres the call came from is        */
+/* written th stderr.                                               */
+/*------------------------------------------------------------------*/
 
 void *T_free( void *ptr )
 {
 #if defined FSC2_MDEBUG
-	int *EBP;           /* assumes sizeof( int ) equals size of pointers */
+	int *EBP;             /* assumes sizeof(int) equals size of pointers */
 #endif
 
 	if ( ptr == NULL )
@@ -197,8 +231,13 @@ void *T_free( void *ptr )
 }
 
 
-/*---------------------------------------------------------------------*/
-/*---------------------------------------------------------------------*/
+/*--------------------------------------------------------------------*/
+/* Replacement for strdup() (returning NULL if the argument is NULL), */
+/* throwing an OUT_OF_MEMORY_EXCEPTION if strdup() does not succeed.  */
+/* If FSC2_MDEBUG writes out information about the address of the     */
+/* new memory string, the amount of memory allocated for the string   */
+/* and the address the call came from to stderr.                      */
+/*--------------------------------------------------------------------*/
 
 char *T_strdup( const char *str )
 {
@@ -206,7 +245,7 @@ char *T_strdup( const char *str )
 	size_t len;
 
 #if defined FSC2_MDEBUG
-	int *EBP;           /* assumes sizeof( int ) equals size of pointers */
+	int *EBP;             /* assumes sizeof(int) equals size of pointers */
 #endif
 
 
@@ -267,43 +306,6 @@ long T_atol( const char *txt )
 	}
 
 	if ( end_p == ( char * ) txt )
-	{
-		print( FATAL, "Not an integer number: %s.\n", txt );
-		THROW( EXCEPTION );
-	}
-
-	return ret;
-}
-
-
-/*---------------------------------------------------------------------*/
-/*---------------------------------------------------------------------*/
-
-long T_htol( const char *txt )
-{
-	long ret;
-	char *end_p;
-
-
-#ifndef NDEBUG
-	if ( txt == NULL || *txt == '\0' ||
-		 ( *txt != '0' && tolower( txt[ 1 ] ) != 'x' ) )
-	{
-		eprint( FATAL, UNSET, "Internal error detected at %s:%d.\n",
-				__FILE__, __LINE__ );
-		THROW( EXCEPTION );
-	}
-#endif
-
-	ret = strtol( txt + 2, &end_p, 16 );
-
-	if ( errno == ERANGE )
-	{
-		print( FATAL, "Long integer number out of range: %s.\n", txt );
-		THROW( EXCEPTION );
-	}
-
-	if ( end_p == ( char * ) ( txt + 2 ) )
 	{
 		print( FATAL, "Not an integer number: %s.\n", txt );
 		THROW( EXCEPTION );
