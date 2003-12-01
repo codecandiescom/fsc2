c33140c45b2449a6871135f347664ffb7d2b9fd0
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2003-12-01 19:57:25 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@6553 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/modules/rb_pulser_init.c
+++ trunk/fsc2/modules/rb_pulser_init.c
@@ -125,14 +125,6 @@ static void rb_pulser_basic_functions_check( void )
 	{
 		f = rb_pulser.function + i;
 
-		if ( f->num_pulses == 0 )
-		{
-			if(  f->is_declared )
-				print( WARN, "No pulses have been assigned to function "
-					   "'%s'.\n", f->name );
-			f->is_used = UNSET;
-		}
-
 		if ( ! f->is_used )
 			continue;
 
@@ -148,6 +140,14 @@ static void rb_pulser_basic_functions_check( void )
 			f->pulses = PULSE_PP T_realloc( f->pulses, f->num_pulses
 													   * sizeof *f->pulses );
 			f->pulses[ f->num_pulses - 1 ] = cp;
+		}
+
+		if ( f->num_pulses == 0 )
+		{
+			if(  f->is_declared )
+				print( WARN, "No pulses have been assigned to function "
+					   "'%s'.\n", f->name );
+			f->is_used = UNSET;
 		}
 	}
 }
