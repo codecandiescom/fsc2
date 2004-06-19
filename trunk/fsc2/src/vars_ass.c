77bdce12c7db1e2288d9cbf16759f7487b65371c
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2004-06-19 19:11:25 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@7038 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/vars_ass.c
+++ trunk/fsc2/src/vars_ass.c
@@ -28,19 +28,19 @@
 /* locally used functions */
 
 
-static void vars_assign_to_1d( Var *src, Var *dest );
-static void vars_assign_to_nd( Var *src, Var *dest );
-static void vars_assign_to_nd_from_1d( Var *src, Var *dest );
-static void vars_assign_to_snd_from_1d( Var *src, Var *dest, Var *sub );
+static long vars_assign_to_1d( Var *src, Var *dest );
+static long vars_assign_to_nd( Var *src, Var *dest );
+static long vars_assign_to_nd_from_1d( Var *src, Var *dest );
+static long vars_assign_to_snd_from_1d( Var *src, Var *dest, Var *sub );
 static long vars_assign_to_snd_range_from_1d( Var *dest, Var *subm,
 											  ssize_t cur, long l, double d );
 static long vars_set_all( Var *v, long l, double d );
-static void vars_assign_to_nd_from_nd( Var *src, Var *dest );
+static long vars_assign_to_nd_from_nd( Var *src, Var *dest );
 static void vars_size_check( Var *src, Var *dest );
-static void vars_assign_to_snd_from_nd( Var *src, Var *dest, Var *sub );
-static void vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
+static long vars_assign_to_snd_from_nd( Var *src, Var *dest, Var *sub );
+static long vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
 										   Var *src );
-static void vars_arr_assign( Var *src, Var *dest );
+static long vars_arr_assign( Var *src, Var *dest );
 
 
 /*--------------------------------------------------------*/
@@ -49,6 +49,9 @@ static void vars_arr_assign( Var *src, Var *dest );
 
 void vars_assign( Var *src, Var *dest )
 {
+	long count = 0;
+
+
 #ifndef NDEBUG
 	/* Check that both variables exist and src has a reasonable type */
 
@@ -84,13 +87,13 @@ void vars_assign( Var *src, Var *dest )
 		case UNDEF_VAR :
 		case INT_VAR : case FLOAT_VAR :
 		case INT_PTR : case FLOAT_PTR :
-			vars_assign_to_1d( src, dest );
+			count = vars_assign_to_1d( src, dest );
 			break;
 
 		case INT_ARR : case FLOAT_ARR :
 		case INT_REF : case FLOAT_REF :
 		case SUB_REF_PTR: case REF_PTR :
-			vars_assign_to_nd( src, dest );
+			count = vars_assign_to_nd( src, dest );
 			break;
 
 #ifndef NDEBUG
@@ -104,7 +107,9 @@ void vars_assign( Var *src, Var *dest )
 	vars_pop( dest );
 	vars_pop( src );
 
-	return;
+	if ( count == 0 )
+		print( WARN, "No assignments could be done, size of left hand side "
+			   "array is unknown.\n" );
 }
 
 
@@ -112,7 +117,7 @@ void vars_assign( Var *src, Var *dest )
 /* Assignment to a variable or a single element of an array */
 /*----------------------------------------------------------*/
 
-static void vars_assign_to_1d( Var *src, Var *dest )
+static long vars_assign_to_1d( Var *src, Var *dest )
 {
 	/* If the destination variable is still completely new set its type */
 
@@ -191,6 +196,8 @@ static void vars_assign_to_1d( Var *src, Var *dest )
 		default :
 			fsc2_assert( 1 == 0 );
 	}
+
+	return 1;
 }
 
 
@@ -198,8 +205,10 @@ static void vars_assign_to_1d( Var *src, Var *dest )
 /* Assignment to an one- or more-dimensional array */
 /*-------------------------------------------------*/
 
-static void vars_assign_to_nd( Var *src, Var *dest )
+static long vars_assign_to_nd( Var *src, Var *dest )
 {
+	long count = 0;
+
 	switch ( src->type )
 	{
 		case INT_VAR : case FLOAT_VAR :
@@ -207,16 +216,20 @@ static void vars_assign_to_nd( Var *src, Var *dest )
 			{
 				case INT_ARR : case FLOAT_ARR :
 				case INT_REF : case FLOAT_REF :
-					vars_assign_to_nd_from_1d( src, dest );
+					count = vars_assign_to_nd_from_1d( src, dest );
 					break;
 
 				case SUB_REF_PTR :
-					vars_assign_to_snd_from_1d( src, dest->from, dest );
+					count = vars_assign_to_snd_from_1d( src, dest->from,
+														dest );
+					break;
+
+				case REF_PTR :
+					count = vars_assign_to_nd_from_1d( src, dest->from );
 					break;
 
 				default :
-					vars_assign_to_nd_from_1d( src, dest->from );
-					break;
+					fsc2_assert( 1 == 0 );
 			}
 			break;
 
@@ -226,16 +239,20 @@ static void vars_assign_to_nd( Var *src, Var *dest )
 			{
 				case INT_ARR : case FLOAT_ARR :
 				case INT_REF : case FLOAT_REF :
-					vars_assign_to_nd_from_nd( src, dest );
+					count = vars_assign_to_nd_from_nd( src, dest );
 					break;
 
 				case SUB_REF_PTR :
-					vars_assign_to_snd_from_nd( src, dest->from, dest );
+					count = vars_assign_to_snd_from_nd( src, dest->from,
+														dest );
+					break;
+
+				case REF_PTR :
+					count = vars_assign_to_nd_from_nd( src, dest->from );
 					break;
 
 				default :
-					vars_assign_to_nd_from_nd( src, dest->from );
-					break;
+					fsc2_assert( 1 == 0 );
 			}
 			break;
 
@@ -246,6 +263,8 @@ static void vars_assign_to_nd( Var *src, Var *dest )
 			THROW( EXCEPTION );
 #endif
 	}
+
+	return count;
 }
 
 
@@ -255,7 +274,7 @@ static void vars_assign_to_nd( Var *src, Var *dest )
 /* fixed size and has no defined elements yet).                     */
 /*------------------------------------------------------------------*/
 
-static void vars_assign_to_nd_from_1d( Var *src, Var *dest )
+static long vars_assign_to_nd_from_1d( Var *src, Var *dest )
 {
 	long lval = 0;
 	double dval = 0.0;
@@ -289,9 +308,7 @@ static void vars_assign_to_nd_from_1d( Var *src, Var *dest )
 		THROW( EXCEPTION );
 	}
 
-	if ( vars_set_all( dest, lval, dval ) == 0 )
-		print( SEVERE, "No elements were set in assignment because size of "
-			   "destination isn't known yet.\n" );
+	return vars_set_all( dest, lval, dval );
 }
 
 
@@ -329,7 +346,7 @@ static long vars_set_all( Var *v, long l, double d )
 /*------------------------------------------------------------------*/
 /*------------------------------------------------------------------*/
 
-static void vars_assign_to_snd_from_1d( Var *src, Var *dest, Var *sub )
+static long vars_assign_to_snd_from_1d( Var *src, Var *dest, Var *sub )
 {
 	long lval = 0;
 	double dval = 0.0;
@@ -355,9 +372,7 @@ static void vars_assign_to_snd_from_1d( Var *src, Var *dest, Var *sub )
 			dval = src->val.dval;
 	}
 
-	if ( vars_assign_to_snd_range_from_1d( dest, sub, 0, lval, dval ) == 0 )
-		print( SEVERE, "No elements were set in assignment because size of "
-			   "destination isn't known yet.\n" );
+	return vars_assign_to_snd_range_from_1d( dest, sub, 0, lval, dval );
 }
 
 
@@ -417,9 +432,10 @@ static long vars_assign_to_snd_range_from_1d( Var *dest, Var *sub,
 /* Assign of an array to an array */
 /*--------------------------------*/
 
-static void vars_assign_to_nd_from_nd( Var *src, Var *dest )
+static long vars_assign_to_nd_from_nd( Var *src, Var *dest )
 {
 	ssize_t i;
+	long count = 0;
 
 
 	/* The dimensions of the destination array must be at least as large as
@@ -436,15 +452,15 @@ static void vars_assign_to_nd_from_nd( Var *src, Var *dest )
 	{
 		for ( i = 0; i < dest->len; i++ )
 			if ( dest->val.vptr[ i ] != NULL )
-				vars_assign_to_nd_from_nd( src, dest->val.vptr[ i ] );
-		return;
+				count += vars_assign_to_nd_from_nd( src, dest->val.vptr[ i ] );
+		return count;
 	}
 
 	/* Unless the destination array is dynamically sized the sizes of
 	   both arrays must be identical */
 
 	vars_size_check( src, dest );
-	vars_arr_assign( src, dest );
+	return vars_arr_assign( src, dest );
 }
 
 
@@ -479,9 +495,10 @@ static void vars_size_check( Var *src, Var *dest )
 /* Really assign one array to another by looping over all elements. */
 /*------------------------------------------------------------------*/
 
-static void vars_arr_assign( Var *src, Var *dest )
+static long vars_arr_assign( Var *src, Var *dest )
 {
 	ssize_t i;
+	long count = 0;
 
 
 	/* If we're at the lowest level, i.e. are dealing with one-dimensional
@@ -502,7 +519,7 @@ static void vars_arr_assign( Var *src, Var *dest )
 				print( WARN, "Assignment from 1D-array that has no "
 					   "elements.\n" );
 				dest->len = 0;
-				return;
+				return 0;
 			}
 
 			if ( dest->len != 0 )
@@ -544,6 +561,9 @@ static void vars_arr_assign( Var *src, Var *dest )
 				memcpy( dest->val.dpnt, src->val.dpnt,
 						dest->len * sizeof *dest->val.dpnt );
 		}
+
+
+		count = dest->len;
 	}
 	else
 	{
@@ -594,8 +614,11 @@ static void vars_arr_assign( Var *src, Var *dest )
 		/* Now copy the sub-arrays by calling the function recursively */
 
 		for ( i = 0; i < dest->len; i++ )
-			vars_arr_assign( src->val.vptr[ i ], dest->val.vptr[ i ] );
+			count += vars_arr_assign( src->val.vptr[ i ],
+									  dest->val.vptr[ i ] );
 	}
+
+	return count;
 }
 
 
@@ -603,7 +626,7 @@ static void vars_arr_assign( Var *src, Var *dest )
 /* Assign of an array to an array specified with ranges */
 /*------------------------------------------------------*/
 
-static void vars_assign_to_snd_from_nd( Var *src, Var *dest, Var *sub )
+static long vars_assign_to_snd_from_nd( Var *src, Var *dest, Var *sub )
 {
 	ssize_t cur = 0;
 	ssize_t single_indices = 0;
@@ -631,68 +654,43 @@ static void vars_assign_to_snd_from_nd( Var *src, Var *dest, Var *sub )
 		THROW ( EXCEPTION );
 	}
 
-	vars_assign_snd_range_from_nd( dest, sub, cur, src );
+	return vars_assign_snd_range_from_nd( dest, sub, cur, src );
 }
 
 
 /*--------------------------------------------*/
 /*--------------------------------------------*/
 
-static void vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
+static long vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
 										   Var *src )
 {
 	ssize_t range_start, range_end, range, i, ind;
-	Var *nsrc, *ndest;
+	long count = 0;
 
 
-	if ( cur == sub->len )
-	{
-		switch ( src->type )
-		{
-			case INT_PTR :
-				nsrc = vars_push( INT_VAR, *src->val.lpnt );
-				break;
-
-			case FLOAT_PTR :
-				nsrc = vars_push( FLOAT_VAR, *src->val.dpnt );
-				break;
-
-			default :
-				nsrc = vars_push_copy( src );
-				break;
-		}
-
-		if ( dest->type & ( INT_PTR | FLOAT_PTR ) )
-			ndest = dest;
-		else
-			ndest = vars_push( REF_PTR, dest );
-
-		vars_assign( nsrc, ndest );
-		return;
-	}
-	
 	ind = sub->val.index[ cur++ ];
 
 	if ( ind >= 0 )
-	{
 		switch ( dest->type )
 		{
 			case INT_ARR :
-				ndest = vars_push( INT_PTR, dest->val.lpnt + ind );
-				break;
+				return vars_assign_to_1d( src,
+										  vars_push( INT_PTR,
+													 dest->val.lpnt + ind ) );
 
 			case FLOAT_ARR :
-				ndest = vars_push( FLOAT_PTR, dest->val.dpnt + ind );
-				break;
+				return vars_assign_to_1d( src,
+										  vars_push( FLOAT_PTR,
+													 dest->val.dpnt + ind ) );
 
 			default :
-				ndest = dest->val.vptr[ ind ];
-				break;
+				if ( cur == sub->len )
+					return vars_assign_to_nd( src, dest->val.vptr[ ind ] );
+				else
+					return vars_assign_snd_range_from_nd(
+														 dest->val.vptr[ ind ],
+														 sub, cur, src );
 		}
-
-		vars_assign_snd_range_from_nd( ndest, sub, cur, src );
-		return;
-	}
 
 	range_start = - ind - 1;
 	range_end = sub->val.index[ cur++ ];
@@ -717,6 +715,7 @@ static void vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
 					else
 						for ( i = range_start; i <= range_end; i++ )
 							dest->val.lpnt[ i ] = round( *src->val.dpnt );
+					count = 1;
 					break;
 
 				case FLOAT_ARR :
@@ -726,11 +725,12 @@ static void vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
 					else
 						for ( i = range_start; i <= range_end; i++ )
 							dest->val.dpnt[ i ] = *src->val.dpnt;
+					count = 1;
 					break;
 
 				default :
 					for ( i = 0; i < range; i++ )
-						vars_assign_snd_range_from_nd(
+						count += vars_assign_snd_range_from_nd(
 											 dest->val.vptr[ i + range_start ],
 											 sub, cur, src->val.vptr[ i ] );
 					break;
@@ -755,6 +755,7 @@ static void vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
 						for ( i = 0; i < range; i++ )
 							dest->val.lpnt[ i + range_start ] =
 												   round( src->val.dpnt[ i ] );
+					count = range;
 					break;
 
 				case FLOAT_ARR :
@@ -765,32 +766,26 @@ static void vars_assign_snd_range_from_nd( Var *dest, Var *sub, ssize_t cur,
 					else
 						memcpy( dest->val.dpnt + range_start, src->val.dpnt,
 								range * sizeof *dest->val.dpnt );
+					count = range;
 					break;
 
 				default :
-					for ( i = 0; i < range; i++ )
-					{
-						if ( src->type == INT_ARR )
-							nsrc = vars_push( INT_PTR, src->val.lpnt + i );
-						else
-							nsrc = vars_push( FLOAT_PTR, src->val.dpnt + i );
-						vars_assign_snd_range_from_nd(
-											 dest->val.vptr[ i + range_start ],
-											 sub, cur, nsrc );
-						if ( src->type & ( INT_ARR | FLOAT_ARR ) )
-							vars_pop( nsrc );
-					}
+					for ( i = range_start; i <= range_end; i++ )
+						count += vars_assign_snd_range_from_nd(
+														   dest->val.vptr[ i ],
+														   sub, cur, src );
 					break;
 			}
 			break;
 
 		default :
-			for ( i = 0; i < range; i++ )
-				vars_assign_snd_range_from_nd(
-											 dest->val.vptr[ i + range_start ],
-											 sub, cur, src );
+			for ( i = range_start; i <= range_end; i++ )
+				count += vars_assign_snd_range_from_nd( dest->val.vptr[ i ],
+														sub, cur, src );
 			break;
 	}
+
+	return count;
 }
 
 
