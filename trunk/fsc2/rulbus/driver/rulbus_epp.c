43d4d9d5bc0c26ec4bc96a7614e371900934d533
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2004-08-31 16:21:20 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@7194 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/rulbus/driver/rulbus_epp.c
+++ trunk/fsc2/rulbus/driver/rulbus_epp.c
@@ -1,28 +1,28 @@
 /*
-  $Id$
- 
-  Driver for the RULBUS EPP interface
-
-  Copyright (C) 2003-2004 Jens Thoms Toerring
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2, or (at your option)
-  any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; see the file COPYING.  If not, write to
-  the Free Software Foundation, 59 Temple Place - Suite 330,
-  Boston, MA 02111-1307, USA.
-
-  To contact the author send email to
-  Jens.Toerring@physik.fu-berlin.de
-*/
+ *  $Id$
+ * 
+ *  Driver for the RULBUS EPP interface
+ *
+ *  Copyright (C) 2003-2004 Jens Thoms Toerring
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ *
+ *  To contact the author send email to
+ *  Jens.Toerring@physik.fu-berlin.de
+ */
 
 
 #include <linux/config.h>
@@ -576,12 +576,15 @@ static int rulbus_epp_init( void )
 
         outb( SPP_nReset, CTRL_BYTE );
 
-        /* Reset the device by toggling the !reset bit */
+        /* Reset the device by toggling the !reset bit - afterwards always
+		   wait a bit for the RC-network to follow the reset line (thanks
+		   to Marin Moene for sending in a patch to do that correctly). */
 
         ctrl = inb( CTRL_BYTE );
         outb( ctrl & ~ SPP_nReset, CTRL_BYTE );
 		udelay( 1000 );
         outb( ctrl | SPP_nReset, CTRL_BYTE );
+		udelay( 1000 );
 
 		/* Finally check if the interface can be detected */
 
