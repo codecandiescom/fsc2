1404fe1855ef3786757d331b2749480989df3d8f
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2004-10-26 08:13:03 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@7233 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/rulbus/include/rulbus_lib.h
+++ trunk/fsc2/rulbus/include/rulbus_lib.h
@@ -73,18 +73,15 @@ typedef enum
 #endif
 
 
-#define RULBUS_UNIPOLAR       0
-#define RULBUS_BIPOLAR        1
-
-
-#define RULBUS_DEF_RACK_ADDR      0x0F
-#define RULBUS_INV_RACK_ADDR      0xFF
-#define RULBUS_MIN_CARD_ADDR      0x01
-#define RULBUS_MAX_CARD_ADDR      0xfe
-#define RULBUS_INV_CARD_ADDR      0xff
+#define RULBUS_DEF_RACK_ADDR                   0x0F
+#define RULBUS_INV_RACK_ADDR                   0xFF
+#define RULBUS_MIN_CARD_ADDR                   0x01
+#define RULBUS_MAX_CARD_ADDR                   0xfe
+#define RULBUS_INV_CARD_ADDR                   0xff
 
 
 #define RULBUS_RB_GENERIC_DEF_ADDR             0x00
+
 
 #define RULBUS_RB8509_ADC12_DEF_ADDR           0xC0
 #define RULBUS_RB8509_ADC12_WIDTH              2
@@ -94,17 +91,24 @@ typedef enum
 #define RULBUS_RB8509_ADC12_DEF_EXT_TRIGGER    0
 #define RULBUS_RB8509_ADC12_MAX_CHANNELS       8
 
+
 #define RULBUS_RB8510_DAC12_DEF_ADDR           0xD0
 #define RULBUS_RB8510_DAC12_WIDTH              2
 #define RULBUS_RB8510_DAC12_DEF_BIPOLAR        1
 #define RULBUS_RB8510_DAC12_DEF_VPB            5.0e-3
 
+
 #define RULBUS_RB8514_DELAY_DEF_ADDR           0xC4
 #define RULBUS_RB8514_DELAY_WIDTH              4
 #define RULBUS_RB8514_DELAY_DEF_INTR_DELAY     6.0e-8
 
+
 #define RULBUS_RB8515_CLOCK_DEF_ADDR           0xC8
 #define RULBUS_RB8515_CLOCK_WIDTH              1
+
+
+#define RULBUS_UNIPOLAR                        0
+#define RULBUS_BIPOLAR                         1
 
 
 typedef struct RULBUS_CARD_LIST RULBUS_CARD_LIST;
@@ -140,12 +144,14 @@ void rulbus_rb8509_adc12_exit( void );
 int rulbus_rb8509_adc12_card_init( int handle );
 void rulbus_rb8509_adc12_card_exit( int handle );
 
+
 /* Internal functions for 12-bit DAC cards (RB8510) */
 
 int rulbus_rb8510_dac12_init( void );
 void rulbus_rb8510_dac12_exit( void );
 int rulbus_rb8510_dac12_card_init( int handle );
 void rulbus_rb8510_dac12_card_exit( int handle );
+
 
 /* Internal functions for delay cards (RB8514) */
 
@@ -154,12 +160,14 @@ void rulbus_rb8514_delay_exit( void );
 int rulbus_rb8514_delay_card_init( int handle );
 void rulbus_rb8514_delay_card_exit( int handle );
 
+
 /* Internal functions for clock cards (RB8515) */
 
 int rulbus_rb8515_clock_init( void );
 void rulbus_rb8515_clock_exit( void );
 int rulbus_rb8515_clock_card_init( int handle );
 void rulbus_rb8515_clock_card_exit( int handle );
+
 
 /* Internal functions for generic cards (RB_GENERIC) */
 
