f3cef184cc8d2108d5efdcc21d55fbde00ff087f
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2005-03-11 16:00:34 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@7297 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/pulser.h
+++ trunk/fsc2/src/pulser.h
@@ -38,49 +38,62 @@ struct Pulser_Struct {
 	bool needs_phase_pulses;
 	bool has_pods;
 
-	bool ( *assign_function )( int function, long connector );
-	bool ( *assign_channel_to_function )( int function, long channel );
-	bool ( *invert_function )( int function );
-	bool ( *set_function_delay )( int function, double delay );
-	bool ( *set_function_high_level )( int function, double high_voltage );
-	bool ( *set_function_low_level )( int function, double low_voltage );
+	bool ( *assign_function )( int /* function */, long /* connector */ );
+	bool ( *assign_channel_to_function )( int /* function */,
+										  long /* channel */ );
+	bool ( *invert_function )( int /* function */ );
+	bool ( *set_function_delay )( int /* function */, double /* delay */ );
+	bool ( *set_function_high_level )( int /* function */,
+									   double /* high_voltage */ );
+	bool ( *set_function_low_level )( int /* function */,
+									  double /* low_voltage */ );
+	bool ( *set_timebase )( double /* timebase */ );
+	bool ( *set_timebase_level ) ( int /* level_type */ );
+	bool ( *set_trigger_mode )( int /* mode */ );
+	bool ( *set_repeat_time )( double /* rep_time */ );
+	bool ( *set_trig_in_level )( double /* voltage */ );
+	bool ( *set_trig_in_slope )( int /* slope */ );
+	bool ( *set_trig_in_impedance )( int /* state */ );
 
-	bool ( *set_timebase )( double timebase );
-	bool ( *set_timebase_level ) ( int level_type );
-	bool ( *set_trigger_mode )( int mode );
-	bool ( *set_repeat_time )( double rep_time );
-	bool ( *set_trig_in_level )( double voltage );
-	bool ( *set_trig_in_slope )( int slope );
-	bool ( *set_trig_in_impedance )( int state );
+	bool ( *set_phase_reference )( int /* phase */, int /* function */ );
 
-	bool ( *set_phase_reference )( int phase, int function );
+	bool ( *new_pulse )( long /* pulse_number */ );
+	bool ( *set_pulse_function )( long /* pulse_number */,
+								  int /* function */ );
+	bool ( *set_pulse_position )( long /* pulse_number */,
+								  double /* ptime */ );
+	bool ( *set_pulse_length )( long /* pulse_number */, double /* ptime */ );
+	bool ( *set_pulse_position_change )( long /* pulse_number */,
+										 double /* ptime */ );
+	bool ( *set_pulse_length_change )( long /* pulse_number */
+									   double /* ptime */ );
+	bool ( *set_pulse_phase_cycle )( long /* pulse_number */,
+									 long /* cycle */ );
+	bool ( *get_pulse_function )( long /* pulse_number */,
+								  int * /* function */ );
+	bool ( *get_pulse_position )( long /* pulse_number */,
+								  double * /* ptime */ );
+	bool ( *get_pulse_length )( long /* pulse_number */,
+								double * /* ptime */ );
+	bool ( *get_pulse_position_change )( long /* pulse_number */,
+										 double * /* ptime */ );
+	bool ( *get_pulse_length_change )( long /* pulse_number */,
+									   double * /* ptime */ );
+	bool ( *get_pulse_phase_cycle )( long /* pulse_number */,
+									 long * /* cycle */ );
+	bool ( *phase_setup_prep )( int /* func */, int /* type */,
+								int /* pod */, long /* val */ );
+	bool ( *phase_setup )( int /* func */ );
 
-	bool ( *new_pulse )( long pulse_number );
-	bool ( *set_pulse_function )( long pulse_number, int function );
-	bool ( *set_pulse_position )( long pulse_number, double ptime );
-	bool ( *set_pulse_length )( long pulse_number, double ptime );
-	bool ( *set_pulse_position_change )( long pulse_number, double ptime );
-	bool ( *set_pulse_length_change )( long pulse_number, double ptime );
-	bool ( *set_pulse_phase_cycle )( long pulse_number, long cycle );
-
-	bool ( *get_pulse_function )( long pulse_number, int *function );
-	bool ( *get_pulse_position )( long pulse_number, double *ptime );
-	bool ( *get_pulse_length )( long pulse_number, double *ptime );
-	bool ( *get_pulse_position_change )( long pulse_number, double *ptime );
-	bool ( *get_pulse_length_change )( long pulse_number, double *ptime );
-	bool ( *get_pulse_phase_cycle )( long pulse_number, long *cycle );
-
-	bool ( *phase_setup_prep )( int func, int type, int pod, long val );
-	bool ( *phase_setup )( int func );
-
-	long ( *ch_to_num )( long channel );
+	long ( *ch_to_num )( long /* channel */ );
 
 	/* The remaining functions are for backward compatibility only */
 
-	bool ( *set_max_seq_len )( double seq_len );
+	bool ( *set_max_seq_len )( double /* seq_len */ );
 	bool ( *keep_all_pulses )( void );
-	bool ( *set_phase_switch_delay )( int function, double del_time );
-	bool ( *set_grace_period )( double gp_time );
+	bool ( *set_phase_switch_delay )( int /* function */,
+									  double /* del_time */ );
+	bool ( *set_grace_period )( double /* gp_time */ );
 };
 
 
@@ -103,36 +116,37 @@ struct P_List {
 
 void pulser_struct_init( void );
 void pulser_cleanup( void );
-void p_assign_pod( long func, Var_T *v );
-void p_assign_channel( long func, Var_T *v );
-void p_set_delay( long func, Var_T *v );
-void p_inv( long func );
-void p_set_v_high( long func, Var_T *v );
-void p_set_v_low( long func, Var_T *v );
-void p_set_timebase( Var_T *v );
-void p_set_timebase_level( int level_type );
-void p_set_trigger_mode( Var_T *v);
-void p_set_trigger_slope( Var_T *v );
-void p_set_trigger_level( Var_T *v );
-void p_set_trigger_impedance( Var_T *v );
-void p_set_rep_time( Var_T *v );
-void p_set_rep_freq( Var_T *v );
-void p_phase_ref( int function, int ref );
-long p_num( char *txt );
-long p_new( long pnum );
-void p_set( long pnum, int type, Var_T *v );
-Var_T *p_get( char *txt, int type );
-Var_T *p_get_by_num( long pnum, int type );
-void p_phs_setup( int func, int type, int pod, long val, bool is_pod );
-void p_phs_end( int func );
-void p_exists_function( int function );
-long p_ch2num( long channel );
+void p_assign_pod( long /* func */, Var_T * /* v */ );
+void p_assign_channel( long /* func */, Var_T * /* v */ );
+void p_set_delay( long /* func */, Var_T * /* v */ );
+void p_inv( long /* func */ );
+void p_set_v_high( long /* func */, Var_T * /* v */ );
+void p_set_v_low( long /* func */, Var_T * /* v */ );
+void p_set_timebase( Var_T * /* v */ );
+void p_set_timebase_level( int /* level_type */ );
+void p_set_trigger_mode( Var_T * /* v */);
+void p_set_trigger_slope( Var_T * /* v */ );
+void p_set_trigger_level( Var_T * /* v */ );
+void p_set_trigger_impedance( Var_T * /* v */ );
+void p_set_rep_time( Var_T * /* v */ );
+void p_set_rep_freq( Var_T * /* v */ );
+void p_phase_ref( int /* function */, int /* ref */ );
+long p_num( char * /* txt */ );
+long p_new( long /* pnum */ );
+void p_set( long /* pnum */, int /* type */, Var_T * /* v */ );
+Var_T *p_get( char * /* txt */, int /* type */ );
+Var_T *p_get_by_num( long /* pnum */, int /* type */ );
+void p_phs_setup( int /* func */, int /* type */, int /* pod */,
+				  long /* val */, bool /* is_pod */ );
+void p_phs_end( int /* func */ );
+void p_exists_function( int /* function */ );
+long p_ch2num( long /* channel */ );
 
 /* The following functions are for backward compatibility only */
 
-void p_set_psd( int func, Var_T *v );
-void p_set_gp( Var_T *v );
-void p_set_max_seq_len( Var_T *v );
+void p_set_psd( int /* func */, Var_T * /* v */ );
+void p_set_gp( Var_T * /* v */ );
+void p_set_max_seq_len( Var_T * /* v */ );
 void keep_all_pulses( void );
 
 
--- trunk/fsc2/src/serial.h
+++ trunk/fsc2/src/serial.h
@@ -30,24 +30,28 @@
 
 /* Routines to be used from modules */
 
-void fsc2_request_serial_port( int sn, const char *dev_name );
-struct termios *fsc2_serial_open( int sn, const char *dev_name, int flags );
-void fsc2_serial_close( int sn );
-ssize_t fsc2_serial_write( int sn, const void *buf, size_t count,
-						   long us_wait, bool quit_on_signal );
-ssize_t fsc2_serial_read( int sn, void *buf, size_t count,
-						  long us_wait, bool quit_on_signal );
-int fsc2_tcgetattr( int sn, struct termios *termios_p );
-int fsc2_tcsetattr( int sn, int optional_actions, struct termios *termios_p );
-int fsc2_tcsendbreak( int sn, int duration );
-int fsc2_tcdrain( int sn );
-int fsc2_tcflush( int sn, int queue_selector );
-int fsc2_tcflow( int sn, int action );
+void fsc2_request_serial_port( int /* sn */, const char * /* dev_name */ );
+struct termios *fsc2_serial_open( int /* sn */, const char * /* dev_name */,
+								  int /* flags */ );
+void fsc2_serial_close( int /* sn */ );
+ssize_t fsc2_serial_write( int /* sn */, const void * /* buf */,
+						   size_t /* count */, long /* us_wait */,
+						   bool /* quit_on_signal */ );
+ssize_t fsc2_serial_read( int /* sn */, void * /* buf */, size_t /* count */,
+						  long /* us_wait */, bool /* quit_on_signal */ );
+int fsc2_tcgetattr( int /* sn */, struct termios * /* termios_p */ );
+int fsc2_tcsetattr( int /* sn */, int /* optional_actions */,
+					struct termios * /* termios_p */ );
+int fsc2_tcsendbreak( int /* sn */, int /* duration */ );
+int fsc2_tcdrain( int /* sn */ );
+int fsc2_tcflush( int /* sn */, int /* queue_selector */ );
+int fsc2_tcflow( int /* sn */, int /* action */ );
 
 /* Routines for internal use */
 
 void fsc2_serial_init( void );
-void fsc2_serial_exp_init( const char *log_file_name, int log_level );
+void fsc2_serial_exp_init( const char * /* log_file_name */,
+						   int /* log_level */ );
 void fsc2_serial_cleanup( void );
 void fsc2_final_serial_cleanup( void );
 
