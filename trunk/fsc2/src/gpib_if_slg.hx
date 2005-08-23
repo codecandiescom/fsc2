3ef70b3adc3dfbc4ce2036c5774b31920d8c2c5f
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2005-08-23 18:24:32 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/svn/fsc2@7494 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/src/gpib_if_slg.hx
+++ trunk/fsc2/src/gpib_if_slg.hx
@@ -44,7 +44,7 @@
 /* The SLG library defines ERR which clashes with newer kernel versions. So
    the following line will tell the user about this potential problem... */
 
-#if ! definded IBERR
+#if ! defined IBERR
 #warning "***************************"
 #warning "* Using ERR will conflict *"
 #warning "* with post-2.2 kernels!  *"
