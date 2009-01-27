1bd95946d337496d0742cb1cfbe88ad6c24e34a9
jens <jens@934aee4a-8510-0410-ae50-ec5b2ea4bb86>
2009-01-27 18:54:16 +0000

    git-svn-id: file:///usr/local/svn/fsc2@8195 934aee4a-8510-0410-ae50-ec5b2ea4bb86

--- trunk/fsc2/ni6601/driver/utils.c
+++ trunk/fsc2/ni6601/driver/utils.c
@@ -1,7 +1,7 @@
 /*
  *  $Id$
  * 
- *  Copyright (C) 2002-2008 Jens Thoms Toerring
+ *  Copyright (C) 2002-2009 Jens Thoms Toerring
  * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
