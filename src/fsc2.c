4271142cc278da275caf5667b8358b2a9fb311b4
Jens Thoms Törring <jt@toerring.de>
2011-06-20 23:30:06 +0200

    Switch to GPL version 3

--- src/fsc2.c
+++ src/fsc2.c
@@ -1,11 +1,11 @@
 /*
- *  Copyright (C) 1999-2010 Jens Thoms Toerring
+ *  Copyright (C) 1999-2011 Jens Thoms Toerring
  *
  *  This file is part of fsc2.
  *
  *  Fsc2 is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  *  Fsc2 is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with fsc2.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
